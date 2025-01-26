4ada0bf03864d50ecf401fc7183ace4276a428be
Roger Perez <rogerprz@github.com>
2025-01-26 15:40:37 -0800

    Time: 6 ms (8.56%), Space: 54.5 MB (34.23%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.js
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.js
@@ -11,13 +11,13 @@
  * @return {boolean}
  */
 var isBalanced = function(root) {
-    // NOT MY SOLUTION
     if (!root) return !root
 
+    const height = (root) => {
+        if (!root) return -1 
 
-    const height = (root) =>{
-        if (!root) return -1
-        return 1 + Math.max(height(root.left), height(root.right));
+        return 1 + Math.max(height(root.left), height( root.right))
     }
+
     return Math.abs(height(root.left) - height(root.right)) < 2 && isBalanced(root.left) && isBalanced(root.right)
-}
+ };
