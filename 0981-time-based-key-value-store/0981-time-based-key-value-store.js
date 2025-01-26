2464f4fda7684b0f6820057379f4d02af9eef6b7
Roger Perez <rogerprz@github.com>
2025-01-26 14:16:12 -0800

    Time: 58 ms (65.51%), Space: 92 MB (8.39%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.js
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.js
@@ -11,9 +11,9 @@ var TimeMap = function() {
  */
 TimeMap.prototype.set = function(key, value, timestamp) {
     if (!this.map.has(key)) {
-        this.map.set(key, [])
+        this.map.set(key,[])
     }
-    this.map.get(key).push([timestamp, value]);
+    this.map.get(key).push([timestamp, value])
 };
 
 /** 
@@ -22,25 +22,25 @@ TimeMap.prototype.set = function(key, value, timestamp) {
  * @return {string}
  */
 TimeMap.prototype.get = function(key, timestamp) {
-    if (!this.map.has(key)) return '';
-    const items = this.map.get(key);
-    let left = 0 
-    let right = items.length - 1;
+    if (!this.map.has(key)) return ''
+    const items = this.map.get(key)
+
+    let l = 0;
+    let r = items.length - 1
     let result = ''
-    
-    while (left <= right) {
-        let mid = Math.floor((left + right) / 2);
+    while (l <= r) {
+        const mid = Math.floor((l + r) / 2)
+
         if (items[mid][0] <= timestamp) {
-            result = items[mid][1];
-            left = mid + 1;
-        } else {
-            right = mid - 1;
+            result = items[mid][1]
+            l = mid + 1
+        }
+        else {
+            r = mid - 1
         }
     }
-
-    return result;
+    return result
 };
-
 
 /** 
  * Your TimeMap object will be instantiated and called as such:
