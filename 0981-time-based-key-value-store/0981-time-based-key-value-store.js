c54f20781fc569e3dbd8aaa3175214548e1b4ae6
Roger Perez <rogerprz@github.com>
2025-01-25 17:50:22 -0800

    Time: 46 ms (99.27%), Space: 89.6 MB (91.44%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.js
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.js
@@ -25,19 +25,18 @@ TimeMap.prototype.get = function(key, timestamp) {
     if (!this.map.has(key)) return '';
     const items = this.map.get(key);
     let left = 0, right = items.length - 1;
+    let result = ''
     while (left <= right) {
-        const mid = Math.floor((left + right) / 2);
-        if (items[mid][0] <= timestamp) {
-            if (mid === items.length - 1 || items[mid + 1][0] > timestamp) {
-                return items[mid][1];
-            } else {
+            let mid = Math.floor((left + right) / 2);
+            if (items[mid][0] <= timestamp) {
+                result = items[mid][1];
                 left = mid + 1;
+            } else {
+                right = mid - 1;
             }
-        } else {
-            right = mid - 1;
         }
-    }
-    return '';
+
+    return result;
 };
 
 
