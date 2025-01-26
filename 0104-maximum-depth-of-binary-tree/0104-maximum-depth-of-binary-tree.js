2a77f80daa8497f1feabf2d40a41da2bab952d79
Roger Perez <rogerprz@github.com>
2025-01-26 15:34:34 -0800

    Time: 0 ms (100%), Space: 52.4 MB (44.63%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
@@ -11,8 +11,7 @@
  * @return {number}
  */
 var maxDepth = function(root) {
-    if (!root) return root 
+    if (!root) return 0
 
-    return Math.max(maxDepth(root.left),  maxDepth(root.right)) + 1
-
+    return 1 + Math.max(maxDepth(root.left), maxDepth(root.right))
 };
