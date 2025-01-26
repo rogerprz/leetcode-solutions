6e7975bf704d0c01a77164339082c6f7cbf62f55
Roger Perez <rogerprz@github.com>
2025-01-26 13:00:34 -0800

    Time: 7 ms (77.14%), Space: 52.6 MB (68.57%) - LeetHub

--- 0616-add-bold-tag-in-string/0616-add-bold-tag-in-string.js
+++ 0616-add-bold-tag-in-string/0616-add-bold-tag-in-string.js
@@ -4,23 +4,31 @@
  * @return {string}
  */
 var addBoldTag = function (s, words) {
+    // "abcxyz123"
+    // [false,...,false] = 9 len
     const bold = new Array(s.length).fill(false)
     // s = "abcxyz123", 
     // words = ["abc","123"]
     for (const word of words) {
-        let start = s.indexOf(word);
+        let start = s.indexOf(word); // 0 index
 
         while (start > -1) {
-            const end = start + word.length;
+            // end of the word length 0 + 3 
+            const end = start + word.length; 
             for (let i = start; i < end; i++) {
+                // replacing false with true based on len
                 bold[i] = true;
             }
+            // we check for the next word in the string
+            //  abcxyzabc123
+            //  check, then index of 6 for next abc
             start = s.indexOf(word, start + 1)
         }
     }
     const res = [];
     const openTag = '<b>';
     const closeTag = '</b>'
+
     for (let i = 0; i < s.length;i++) {
         if (bold[i] && (i === 0 || !bold[i-1])){
              res.push(openTag)
