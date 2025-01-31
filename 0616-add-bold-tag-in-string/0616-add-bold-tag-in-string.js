46608f132dc1e42b3eb4d3193895cda4f213edfb
Roger Perez <rogerprz@github.com>
2025-01-31 08:40:40 -0800

    Time: 12 ms (47.62%), Space: 53.8 MB (42.86%) - LeetHub

--- 0616-add-bold-tag-in-string/0616-add-bold-tag-in-string.js
+++ 0616-add-bold-tag-in-string/0616-add-bold-tag-in-string.js
@@ -3,40 +3,41 @@
  * @param {string[]} words
  * @return {string}
  */
-var addBoldTag = function (s, words) {
-    // "abcxyz123"
-    // [false,...,false] = 9 len
-    const bold = new Array(s.length).fill(false)
-    // s = "abcxyz123", 
-    // words = ["abc","123"]
+var addBoldTag = function(s, words) {
+    // keep track of which chars need to be bolded
+    const mark = Array.from({length: s.length}, ()=> false)
+    // mark chars as bold
+
     for (const word of words) {
-        let start = s.indexOf(word); // 0 index
+        let start = s.indexOf(word)
 
         while (start > -1) {
-            // end of the word length 0 + 3 
-            const end = start + word.length; 
-            for (let i = start; i < end; i++) {
-                // replacing false with true based on len
-                bold[i] = true;
+            const end = start + word.length
+            for (let i = start; i < end;i++) {
+                mark[i] = true
             }
-            // we check for the next word in the string
-            //  abcxyzabc123
-            //  check, then index of 6 for next abc
             start = s.indexOf(word, start + 1)
         }
     }
-    const res = [];
-    const openTag = '<b>';
-    const closeTag = '</b>'
+
+    let res = [];
+    const openTag = "<b>";
+    const closeTag = "</b>";
 
     for (let i = 0; i < s.length;i++) {
-        if (bold[i] && (i === 0 || !bold[i-1])){
-             res.push(openTag)
+        if (mark[i]) {
+            if (i=== 0 || !mark[i-1]) {
+                res.push(openTag)
+            }
         }
         res.push(s[i])
-        if (bold[i] && (i === s.length -1 || !bold[i+1])){
-            res.push(closeTag)
+
+        if (mark[i]) {
+            if (i=== s.length-1 || !mark[i+1]) {
+                res.push(closeTag)
+            }
         }
     }
-    return res.join("")
+
+    return res.join('')
 };
