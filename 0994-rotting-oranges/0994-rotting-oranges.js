af16404559e6a5b1e1dae0dca83aa1b0c155666d
Roger Perez <rogerprz@github.com>
2025-02-01 12:50:06 -0800

    Time: 19 ms (17.01%), Space: 55.4 MB (21.1%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.js
+++ 0994-rotting-oranges/0994-rotting-oranges.js
@@ -3,45 +3,51 @@
  * @return {number}
  */
 var orangesRotting = function(grid) {
-    // 0 empty
-    // 1 orange
-    // 2 rotting 
-    let oranges = 0;
-    let stack = [];
+    /**
+    0 = empty 
+    1 = orange 
+    2 = rotten
+    [2,1,1],
+    [1,1,0],
+    [0,1,1]
+     */
+    //  we want to track fresh oranges 
+    // BFS to make orange rotten. 
+    const queue = []
+    let oranges = 0
     for (let row = 0; row < grid.length;row++) {
-        for (let col = 0; col < grid[0].length;col++) {
+        for (let col = 0; col < grid[0].length; col++) {
             const value = grid[row][col]
+
             if (value === 1) {
                 oranges++
-            }
-            else if (value === 2) {
-                stack.push([row,col, 0])
+            } else if (value === 2) {
+                queue.push([row, col, 0])
             }
         }
     }
-    const deltas = [[0,1],[1,0], [-1, 0], [0, -1]]
-    let maxLevel = 0
-    while (stack.length > 0 && oranges > 0) {
-        const [r, c, level] = stack.shift()
-        const orange = grid[r][c]
-
-        if (orange === 1) {
+    // orangs 6 
+    // [0,0,0]
+    let time = 0
+    const deltas = [[0,1],[1,0],[0,-1],[-1,0]] // N,W,S,E
+    while (queue.length > 0) {
+        const [row, col, mins] = queue.shift()
+        const currValue = grid[row][col]
+        if (currValue === 1) {
+            grid[row][col] = 2
             oranges--
-            grid[r][c] = 2
-            maxLevel = level
+            time = Math.max(mins, time)
         }
-        for (const [row, col] of deltas) {
-            const newRow = r + row
-            const newCol = c + col
-            const rowInbounds = newRow >= 0 && newRow < grid.length 
-            const colInbounds = newCol >= 0 && newCol < grid[0].length;
-
-            if (!rowInbounds || !colInbounds) continue
-
-            if (grid[newRow][newCol] === 1) {
-                stack.push([newRow, newCol, level + 1])
+        for (const [r, c] of deltas) {
+            const nextRow = row + r;
+            const nextCol = col + c
+            // we may need to check outbounds 
+            if (nextRow < 0 || nextRow > grid.length -1) continue
+            if (nextCol < 0 || nextCol > grid[0].length - 1) continue
+            if (grid[nextRow][nextCol]=== 1) {
+                queue.push([nextRow, nextCol, mins + 1])
             }
         }
     }
-    return oranges ? -1 : maxLevel
+    return oranges === 0 ? time : -1
 };
