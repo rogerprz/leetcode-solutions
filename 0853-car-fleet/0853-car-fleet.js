71560b2e376c6952124c67d941584c821f41ebd5
Roger Perez <rogerprz@github.com>
2025-02-02 13:36:39 -0800

    Time: 41 ms (96.58%), Space: 64.2 MB (97.57%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.js
+++ 0853-car-fleet/0853-car-fleet.js
@@ -6,14 +6,20 @@
  */
 var carFleet = function(target, position, speed) {
     const arr = new Array(target).fill(0);
+    
     for (let i = 0; i < position.length; i++) {
-        arr[position[i]] = (target - position[i]) / speed[i];
+        const pos = position[i]
+        const time = (target - pos) / speed[i]
+        arr[pos] = time;
     }
     let fleets = 0, prev = 0;
     for (let i = arr.length - 1; i >= 0; i--) {
-        if (arr[i] <= prev) continue;
-        fleets++;
-        prev = arr[i];
+        const curr = arr[i]
+
+        if (curr > prev){
+            fleets++;
+            prev = curr;
+        }
     }
     return fleets;
 }
