8975abd0752fff6fcb3eb183606e147e65529b38
Roger Perez <rogerprz@github.com>
2025-02-02 12:39:24 -0800

    Time: 12 ms (62.92%), Space: 59.8 MB (66.86%) - LeetHub

--- 0155-min-stack/0155-min-stack.js
+++ 0155-min-stack/0155-min-stack.js
@@ -1,7 +1,7 @@
 
 var MinStack = function() {
     this.stack = []
-
+    this.minStack = []
 };
 
 /** 
@@ -10,27 +10,32 @@ var MinStack = function() {
  */
 MinStack.prototype.push = function(val) {
     this.stack.push(val)
+    const lastMin = this.minStack.length === 0 ? val : this.minStack.at(-1)
+
+    this.minStack.push(Math.min(lastMin, val))
 };
 
 /**
  * @return {void}
  */
 MinStack.prototype.pop = function() {
+    this.minStack.pop()
     return this.stack.pop()
+
 };
 
 /**
  * @return {number}
  */
 MinStack.prototype.top = function() {
-    return this.stack[this.stack.length - 1]
+    return this.stack.at(-1)
 };
 
 /**
  * @return {number}
  */
 MinStack.prototype.getMin = function() {
-    return Math.min(...this.stack)
+    return this.minStack.at(-1)
 };
 
 /** 
