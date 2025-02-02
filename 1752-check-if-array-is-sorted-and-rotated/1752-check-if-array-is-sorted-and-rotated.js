d86d8109f19f9d1ed09a5de4435f00abf96d37a7
Roger Perez <rogerprz@github.com>
2025-02-02 14:01:51 -0800

    Time: 0 ms (100%), Space: 49 MB (60.81%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.js
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.js
@@ -6,8 +6,8 @@ var check = function(nums) {
     let n = nums.length - 1
     let count = 0;
     if (nums[0] < nums[n]) {
-            count++;
-        }
+        count++;
+    }
     for (let i = 0; i< n;i++) {
         if (nums[i] > nums[i+1]) {
             count++
