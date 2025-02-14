72f6ecdf77e12f2b1ca24922e99aa28f109f7274
Roger Perez <rogerprz@github.com>
2025-02-13 22:41:18 -0800

    Time: 91 ms (100%), Space: 70 MB (100%) - LeetHub

--- 3066-minimum-operations-to-exceed-threshold-value-ii/3066-minimum-operations-to-exceed-threshold-value-ii.js
+++ 3066-minimum-operations-to-exceed-threshold-value-ii/3066-minimum-operations-to-exceed-threshold-value-ii.js
@@ -3,23 +3,27 @@
  * @psrsm {number} k
  * @return {number}
  */
-var minOperations = function(nums, k) {
-    nums.sort((a, b) => b- a)
-    let arr = [], operTime = 0, arrPos = 0
-    while((nums.length > 0 && nums[nums.length-1] < k) || (arr.length > 0 && arr[arrPos] < k)) {
-        let min1, min2
-        if(nums[nums.length-1] && !(nums[nums.length-1] > arr[arrPos])) min1 = nums.pop()
-        else { 
-            min1 = arr[arrPos]
-            arrPos ++
+const minOperations = (nums, k) => {
+    nums.sort((x, y) => x - y);
+
+    for (let i = 0, j = 0, count = 0, x, y; ;++count) {
+
+        if (i < nums.length && (nums[i] <= nums[j])) {
+            x = nums[i++];
         }
-        if(nums[nums.length-1] && !(nums[nums.length-1] > arr[arrPos])) min2 = nums.pop()
-        else { 
-            min2 = arr[arrPos]
-            arrPos ++
+        else {
+            x = nums[j++];
         }
-        arr.push(min1*2 + min2)
-        operTime ++
+
+        if (x >= k) return count;
+
+        if (i < nums.length && (j >= count || nums[i] <= nums[j])) {
+            y = nums[i++];
+        }
+        else {
+            y = nums[j++];
+        }
+        nums[count] = x * 2 + y;
     }
-    return operTime
+    return -1;
 };
