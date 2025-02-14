c0119da6bd0ae45c4e21229b8bfa03fea7dd1f1e
Roger Perez <rogerprz@github.com>
2025-02-13 22:39:55 -0800

    Time: 83 ms (100%), Space: 70 MB (100%) - LeetHub

--- 3066-minimum-operations-to-exceed-threshold-value-ii/3066-minimum-operations-to-exceed-threshold-value-ii.js
+++ 3066-minimum-operations-to-exceed-threshold-value-ii/3066-minimum-operations-to-exceed-threshold-value-ii.js
@@ -5,13 +5,25 @@
  */
 const minOperations = (nums, k) => {
     nums.sort((x, y) => x - y);
-    
-    for(let i = 0, j = 0, count = 0, x, y; ; ++count) {
-        if (i < nums.length && (j >= count || nums[i] <= nums[j])) x = nums[i++];
-        else x = nums[j++];
+
+    for (let i = 0, j = 0, count = 0, x, y; ;++count) {
+
+        if (i < nums.length && (j >= count || nums[i] <= nums[j])) {
+            x = nums[i++];
+        }
+        else {
+            x = nums[j++];
+        }
+
         if (x >= k) return count;
-        if (i < nums.length && (j >= count || nums[i] <= nums[j])) y = nums[i++];
-        else y = nums[j++];
+
+        if (i < nums.length && (j >= count || nums[i] <= nums[j])) {
+            y = nums[i++];
+        }
+        else {
+            y = nums[j++];
+        }
         nums[count] = x * 2 + y;
     }
+    return -1;
 };
