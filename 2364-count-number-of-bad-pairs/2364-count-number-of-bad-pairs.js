aff428e831b4bc08757036fccdd913cf33f82951
Roger Perez <rogerprz@github.com>
2025-02-15 12:09:39 -0800

    Time: 29 ms (80.64%), Space: 72.9 MB (61.57%) - LeetHub

--- 2364-count-number-of-bad-pairs/2364-count-number-of-bad-pairs.js
+++ 2364-count-number-of-bad-pairs/2364-count-number-of-bad-pairs.js
@@ -3,7 +3,22 @@
  * @return {number}
  */
 var countBadPairs = function(nums) {
-    const hash = {}
+    const n = nums.length;
+    let badPairs = 0;
+    // Bad: j - i != nums[j] - nums[i] 
+    // Good: nums[i] - i != nums[j] - j
+    const map = new Map()
+    for (let i = 0; i < n; i++) {
+        const diff = i - nums[i]
+
+        const goodPairCount = map.get(diff) || 0;
+        badPairs += i - goodPairCount
+        map.set(diff, goodPairCount + 1)
+    }
+    return badPairs
+};
+/**
+const hash = {}
     let badPairs = 0
     for (let i = 0; i < nums.length; i++) {
         const diff = i - nums[i]
@@ -16,4 +31,19 @@ var countBadPairs = function(nums) {
     }
     
     return badPairs
-};
+
+    const n = nums.length;
+    const map = new Map();
+    // n = 4, 4 * (4 - 1) / 2 = 6
+    let totalPairs = (n * (n - 1)) / 2;
+    let goodPairs = 0;
+
+    for (let i = 0; i < n; i++) {
+        const diff = nums[i] - i;
+
+        goodPairs += map.get(diff) || 0
+        map.set(diff, (map.get(diff) || 0) + 1)
+    }
+
+    return totalPairs - goodPairs;
+     */
