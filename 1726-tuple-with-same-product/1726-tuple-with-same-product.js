acbc56d07c428f5d5ea81c906febe95bac80de7f
Roger Perez <rogerprz@github.com>
2025-02-15 11:38:15 -0800

    Time: 148 ms (57.14%), Space: 93.8 MB (47.02%) - LeetHub

--- 1726-tuple-with-same-product/1726-tuple-with-same-product.js
+++ 1726-tuple-with-same-product/1726-tuple-with-same-product.js
@@ -8,12 +8,14 @@ var tupleSameProduct = function(nums) {
 
     for (let i = 0; i< nums.length;i++) {
         const outer = nums[i]
+        
         for (let j = i + 1; j < nums.length; j++) {
             let inner = nums[j]
-            let product = outer * inner; 
-            const prev = products.get(product) || 0
-            result += 8 * (prev)
-            products.set(product, prev + 1)
+            const total = outer * inner; 
+
+            const prev = products.get(total) || 0
+            result += 8 * prev
+            products.set(total, prev + 1)
         }
     }
 
