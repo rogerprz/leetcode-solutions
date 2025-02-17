9d7db5263b57de501769c9f15508130828d6160c
Roger Perez <rogerprz@github.com>
2025-02-17 13:06:19 -0800

    Time: 12 ms (19.93%), Space: 51.7 MB (35.78%) - LeetHub

--- 0139-word-break/0139-word-break.js
+++ 0139-word-break/0139-word-break.js
@@ -8,15 +8,27 @@ var wordBreak = function(s, wordDict) {
     const queue = [0];
     const seen = new Set()
     while (queue.length > 0) {
+        // Remove the start of queue
         const start = queue.shift()
-
+        // if start is == to len then we are done, return true
+        // this is the end, base end case
         if (start === s.length) return true;
-        
+        // we then loop by checking the end of this subtring
+        // we set start + 1 to end, and go as far as s.len
         for (let end = start + 1; end <= s.length; end++) {
+            // if we've seen end, we continue,
+            // First loop: le in leetcode, we haven't
             if (seen.has(end)) continue;
-
-            if (words.has(s.substring(start,end))) {
+            // we get the substring we need to check
+            const subStr = s.substring(start,end)
+            // Check if subStr is in words, else we increase end to do 
+            // another check
+            if (words.has(subStr)) {
+                // if true, then we push end index
+                // this way we can start checking this in the next while
                 queue.push(end);
+                // we also note that we've seen this index as to not 
+                // test again.
                 seen.add(end)
             }
         }
