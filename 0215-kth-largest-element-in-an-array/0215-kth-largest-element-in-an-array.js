52dea4ca8d002bcde290f79c6cdea53d7a471af3
Roger Perez <rogerprz@github.com>
2025-02-17 12:03:27 -0800

    Time: 19 ms (86.88%), Space: 58.6 MB (86.1%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.js
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.js
@@ -3,25 +3,27 @@
  * @param {number} k
  * @return {number}
  */
-var findKthLargest = function(nums, k) {
-    const max = Math.max(...nums)
+var findKthLargest = function(nums, k) { 
     const min = Math.min(...nums)
-    const array = new Array(max-min+1).fill(0)
+    const max = Math.max(...nums)
 
+    const array =  Array.from({length: max - min + 1}, ()=> 0)
     for (const num of nums) {
         const idx = num - min
         array[idx]++
     }
-
-    for (let i = array.length-1; i>=0; i--) {
-        if (array[i] > 0) {
-            k -= array[i]
+    /**
+          [3,2,1,5,6,4]
+        [0,1,1,1,1,1,1]
+        k = 2
+     */
+    for (let i = array.length - 1; i >=0;i--) {
+        if  (array[i] > 0) {
+            k-= array[i]
             array[i] = 0
         }
-        // while (array[i] > 0){
-        //     array[i]--
-        //     k--
-        // }
-        if (k <=0) return i + min
+        if (k <= 0) {
+            return i + min 
+        }
     }
 };
