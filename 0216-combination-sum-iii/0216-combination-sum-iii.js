d0bb62470a2391c37f6ea9bd6fd17395a648d3dd
Roger Perez <rogerprz@github.com>
2025-02-16 16:21:04 -0800

    Time: 0 ms (100%), Space: 50.6 MB (20.53%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.js
+++ 0216-combination-sum-iii/0216-combination-sum-iii.js
@@ -6,7 +6,31 @@
 var combinationSum3 = function(k, total) {
     const res = []
     
-    function backtrack(arr, sum, start) {
+    function backtrack(arr, start, sum) {
+        if (sum > total) {
+            return 
+        }
+        if (arr.length === k) {
+            if (sum=== total) {
+                res.push(arr)
+            }
+            return 
+        }
+
+        for (let i = start; i < 10; i++) {
+            const newArr = [...arr, i]
+            const newSum = sum + i
+            const newStart = i + 1
+            backtrack(newArr, newStart, newSum)
+        }
+    }
+    backtrack([], 1, 0)
+    return res
+    
+};
+/**
+
+function backtrack(arr, sum, start) {
         if (sum > total) {
             return;
         }
@@ -18,7 +42,7 @@ var combinationSum3 = function(k, total) {
             return;
         }
         
-        for(let i = start; i < 10; i++) {
+        for(let i = start;  < 10; i++) {
             const newArr = [...arr, i]
             const newSum = sum + i;
             backtrack(newArr, newSum, i+1);
@@ -26,8 +50,6 @@ var combinationSum3 = function(k, total) {
     }
     backtrack([], 0, 1);
     return res;
-};
-/**
 var combinationSum3 = function(k, total) {
     const res = [];
 
