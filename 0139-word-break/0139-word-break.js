58c2fc77268b3c121b0750f7441fbbe383e0555b
Roger Perez <rogerprz@github.com>
2025-02-17 13:08:14 -0800

    Time: 3 ms (86.25%), Space: 49.6 MB (96.91%) - LeetHub

--- 0139-word-break/0139-word-break.js
+++ 0139-word-break/0139-word-break.js
@@ -3,36 +3,29 @@
  * @param {string[]} wordDict
  * @return {boolean}
  */
-var wordBreak = function(s, wordDict) {
-    const words = new Set(wordDict)
-    const queue = [0];
-    const seen = new Set()
-    while (queue.length > 0) {
-        // Remove the start of queue
-        const start = queue.shift()
-        // if start is == to len then we are done, return true
-        // this is the end, base end case
-        if (start === s.length) return true;
-        // we then loop by checking the end of this subtring
-        // we set start + 1 to end, and go as far as s.len
-        for (let end = start + 1; end <= s.length; end++) {
-            // if we've seen end, we continue,
-            // First loop: le in leetcode, we haven't
-            if (seen.has(end)) continue;
-            // we get the substring we need to check
-            const subStr = s.substring(start,end)
-            // Check if subStr is in words, else we increase end to do 
-            // another check
-            if (words.has(subStr)) {
-                // if true, then we push end index
-                // this way we can start checking this in the next while
-                queue.push(end);
-                // we also note that we've seen this index as to not 
-                // test again.
-                seen.add(end)
+var wordBreak = function (s, wordDict) {
+    const memo = new Array(s.length).fill(-1);
+    
+    function isValid(i) {
+        if (i < 0) {
+            return true;
+        }
+        if (memo[i] != -1) {
+            return memo[i] === 1;
+        }
+        for (let word of wordDict) {
+            let wordLen = word.length;
+            if (i - wordLen + 1 < 0) continue;
+
+            const isValidSub = s.substring(i - wordLen + 1, i + 1) === word
+            
+            if (isValidSub && isValid(i - wordLen)) {
+                memo[i] = 1;
+                return true;
             }
         }
-
+        memo[i] = 0;
+        return false;
     }
-    return false
+    return isValid(s.length - 1);
 };
