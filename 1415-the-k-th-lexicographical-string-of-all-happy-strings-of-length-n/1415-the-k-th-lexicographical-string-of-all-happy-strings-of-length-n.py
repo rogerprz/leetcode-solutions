ea9e55cff8550c2e636c6a85482c797e5650b983
Roger Perez <rogerprz@github.com>
2025-02-19 09:57:06 -0800

    Time: 0 ms (100%), Space: 17.8 MB (66.08%) - LeetHub

--- 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.py
+++ 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.py
@@ -1,34 +1,22 @@
 class Solution:
     def getHappyString(self, n: int, k: int) -> str:
-        current_string = ""
-        happy_strings = []
-        # Generate all happy strings of length n
-        self.generate_happy_strings(n, current_string, happy_strings)
-
-        # Check if there are at least k happy strings
-        if len(happy_strings) < k:
+        # num of happy strings of len(n) = 3 * 2 ^ (n-1)
+        totalHappy = 3 * (2**(n-1))
+        if k > totalHappy:
             return ""
-
-        # Sort the happy strings in lexicographical order
-        happy_strings.sort()
-
-        return happy_strings[k - 1]
-
-    def generate_happy_strings(
-        self, n: int, current_string: str, happy_strings: list
-    ):
-        # If the current string has reached the desired length, add it to the list
-        if len(current_string) == n:
-            happy_strings.append(current_string)
-            return
-
-        # Try adding each character ('a', 'b', 'c') to the current string
-        for current_char in ["a", "b", "c"]:
-            # Skip if the current character is the same as the last character
-            if len(current_string) > 0 and current_string[-1] == current_char:
-                continue
-
-            # Recursively generate the next character
-            self.generate_happy_strings(
-                n, current_string + current_char, happy_strings
-            )
+        
+        output = ""
+        choices = "abc"
+        low, high = 1, totalHappy
+        for i in range(n):
+            partitionSize = (high - low + 1) // len(choices)
+            cur = low
+            for c in choices:
+                if k in range(cur, cur + partitionSize):
+                    output += c
+                    low = cur
+                    high = cur + partitionSize - 1
+                    choices = "abc".replace(c, "")
+                    break
+                cur += partitionSize
+        return output
