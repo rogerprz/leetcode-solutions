729f6cccd7c784e2d8bd210eefef05db3737686a
Roger Perez <rogerprz@github.com>
2025-02-25 22:13:43 -0800

    Time: 3 ms (100%), Space: 65.5 MB (22.58%) - LeetHub

--- 1524-number-of-sub-arrays-with-odd-sum/1524-number-of-sub-arrays-with-odd-sum.js
+++ 1524-number-of-sub-arrays-with-odd-sum/1524-number-of-sub-arrays-with-odd-sum.js
@@ -7,10 +7,10 @@ var numOfSubarrays = function(arr) {
     let sum = 0;
     let even = 0;
 
-    for(let i = 0;i < arr.length;i++) {
-         sum += arr[i]
-         odd += Number(sum % 2 === 1)
-         even += Number(sum % 2 === 0)   
-        }
+    for (let i = 0;i < arr.length;i++) {
+        sum += arr[i]
+        odd += Number(sum % 2 === 1)
+        even += Number(sum % 2 === 0)   
+    }
     return(odd * even + odd) % (1e9+7)
 };
