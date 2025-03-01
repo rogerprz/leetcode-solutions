8713aabb42b24c1947355e822f0ea4689ab82f2f
Roger Perez <rogerprz@github.com>
2025-03-01 15:22:07 -0800

    Time: 3 ms (6.67%), Space: 53.9 MB (6.67%) - LeetHub

--- 1024-video-stitching/1024-video-stitching.js
+++ 1024-video-stitching/1024-video-stitching.js
@@ -5,11 +5,7 @@
  */
 var videoStitching = function(clips, time) {
     clips.sort((a,b) => {
-        if (a[0] !== b[0]) {
-            return a[0] - b[0];
-        } else {
-            return a[1] - b[1];
-        }
+        return a[0] - b[0];
     })
     if (clips[0][0] !== 0) return -1
 
