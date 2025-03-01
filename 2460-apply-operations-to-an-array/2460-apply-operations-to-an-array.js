643567ffe37ac0a9a369ce2620e33ee01ba065aa
Roger Perez <rogerprz@github.com>
2025-03-01 15:15:14 -0800

    Time: 2 ms (70.19%), Space: 56.6 MB (16.35%) - LeetHub

--- 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.js
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.js
@@ -12,6 +12,7 @@ var applyOperations = function(nums) {
     }
     
     for(let i = 0, j = 0; i < nums.length;i++){
+        const curr = nums[i]
         if (nums[i] != 0){
             [nums[j], nums[i]] = [nums[i],nums[j]]
             j++
