495b112a3479ab1a5b3bb68ebc0fadd4aea9e75e
Roger Perez <rogerprz@github.com>
2025-03-01 16:01:04 -0800

    Time: 3 ms (86.63%), Space: 56.2 MB (15.06%) - LeetHub

--- 0139-word-break/0139-word-break.js
+++ 0139-word-break/0139-word-break.js
@@ -3,39 +3,23 @@
  * @param {string[]} wordDict
  * @return {boolean}
  */
-var wordBreak = function (s, wordDict) {
-    const dp = new Array(s.length).fill(false);
-    let min = Infinity;
-    wordDict.forEach((word) => min = Math.min(min, word.length))
+var wordBreak = function(s, wordDict) {
+    const dp = new Array(s.length).fill(false)
+    // [l,e,e, c,o,]
+    //    i
+    // [lee, co]
     for (let i = 0; i < s.length; i++) {
         for (const word of wordDict) {
-            const len = word.length
-            // We loop through the inner wordDic for each word
-            // to see if it's in the current length
-            // if we don't find it then we increase the i length 
-            // two conditions we check,
-            // 1. if i == end of the word, i.e i = 0, word is 'a'
-            // 2. if the prev location is true, this adds on
-            if (i === len - 1 || dp[i - len]) {
-                const subStr = s.substring(i - len + 1, i + 1)
-                if (subStr === word) {
-                    dp[i] = true;
+            const size = word.length;
+
+            if (i === size - 1 || dp[i - size]) {
+                const subWord = s.substring(i - size + 1, i + 1)
+                if (subWord === word) {
+                    dp[i] = true 
                     break;
                 }
             }
         }
     }
-    return dp[s.length - 1];
+    return dp[s.length - 1]
 };
-
-/**
-
-const len = word.length
-            if (i === len - 1 || dp[i - len]) {
-                const subStr = s.substring(i - len + 1, i + 1)
-                if (subStr == word) {
-                    dp[i] = true;
-                    break;
-                }
-            }
-             */
