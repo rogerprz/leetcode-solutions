155f9ecbcedcc4e1b12875e17094dd7548b68470
Roger Perez <rogerprz@github.com>
2025-03-02 12:02:40 -0800

    Time: 9 ms (22.67%), Space: 63 MB (5.33%) - LeetHub

--- 2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.js
+++ 2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.js
@@ -6,20 +6,37 @@
 var mergeArrays = function(nums1, nums2) {
     // nums1 = [[1,2],[2,3],[4,5]], 
     // nums2 = [[1,4],[3,2],[4,1]]
-    const map = new Map()
+    const array = []
+    let p1 = 0
+    let p2 = 0;
+    while (p1 < nums1.length || p2 < nums2.length) {
 
-    for (const [id,num] of nums1) {
-        map.set(id, num)
-    }
+        if (nums1[p1] && nums2[p2]) { 
+            const [id1,num1] = nums1[p1]
+            const [id2,num2] = nums2[p2]
 
-    for (const [id, num] of nums2) {
-        if (map.has(id)) {
-           const prevNum = map.get(id)
-           map.set(id, prevNum + num)
-        } else {
-            map.set(id, num)
+            if (id1 === id2) {
+                array.push([id1, num1 + num2])
+                p1++
+                p2++
+            }
+            else if (id1 < id2) {
+                array.push([id1, num1])
+                p1++
+            }
+            else {
+                array.push([id2, num2])
+                p2++
+            }
+        } else if (nums1[p1]) {
+            const [id1,num1] = nums1[p1]
+            array.push([id1, num1])
+            p1++
+        } else if (nums2[p2]){
+            const [id2,num2] = nums2[p2]
+            array.push([id2, num2])
+            p2++
         }
     }
-    const sorted = [...map.entries()].sort((a,b) => a[0] - b[0])
-    return sorted
+    return array
 };
