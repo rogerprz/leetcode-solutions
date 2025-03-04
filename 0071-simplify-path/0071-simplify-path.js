2c2bc845d71dd42139670c9be12bd588aff18c4e
Roger Perez <rogerprz@github.com>
2025-03-03 20:55:27 -0800

    Time: 0 ms (100%), Space: 55.3 MB (16.22%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.js
+++ 0071-simplify-path/0071-simplify-path.js
@@ -3,17 +3,26 @@
  * @return {string}
  */
 var simplifyPath = function(path) {
-    const paths = path.split("/")
-    const stack = []
-    for (let i =0;i<paths.length;i++) {
-        const path = paths[i]
-        if (path === "" || path === ".") continue
-        if (path === "..") {
-            stack.pop()
-        } else {
-            stack.push(path)
-        }
-    }
+    
+    // .. 
+    // multi ////
+    // .... are dir names 
+    /**
+    split the array by /
+    -> 
+     */
+     const paths = path.split("/")
+     const stack = [];
 
-    return "/" + stack.join("/")
+     for (const subPath of paths) {
+        if (subPath === '' || subPath === ".") continue 
+
+        if (subPath === "..") {
+            stack.pop()
+            continue
+        }
+        stack.push(subPath)
+     }
+     
+     return '/' + stack.join("/")
 };
