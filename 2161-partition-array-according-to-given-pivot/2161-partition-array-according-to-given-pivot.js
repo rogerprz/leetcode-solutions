2fb22e3a3a39604c4fea05deaadadc97336cb643
Roger Perez <rogerprz@github.com>
2025-03-04 19:45:49 -0800

    Time: 50 ms (9.14%), Space: 87.5 MB (43.3%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.js
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.js
@@ -7,7 +7,7 @@ var pivotArray = function(nums, pivot) {
     let count = 0;
     let less = []
     let great = []
-
+    let arr = new Array(nums.length).fill(0)
     for (const num of nums) {
         if (num === pivot) count++
         else if (pivot < num) {
@@ -16,6 +16,24 @@ var pivotArray = function(nums, pivot) {
             less.push(num)
         }
     }
-    // const pivots = 
-    return [...less, ...new Array(count).fill(pivot), ...great]
+    let i = 0;
+
+    while (i < less.length) {
+        arr[i] = less[i]
+        i++
+    }
+
+    while (i < less.length + count) {
+        arr[i] = pivot
+        i++
+    }
+    let pos = 0
+    while (i < less.length + great.length + count) {
+        arr[i] = great[pos]
+        i++
+        pos++
+    }
+    const result = less.concat(new Array(count).fill(pivot), great)
+
+    return result
 };
