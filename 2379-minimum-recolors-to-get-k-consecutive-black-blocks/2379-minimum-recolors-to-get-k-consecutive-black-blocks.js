3d2dbdff4c9c89616f65ae3d3c43992bdf5491d6
Roger Perez <rogerprz@github.com>
2025-03-08 13:14:17 -0800

    Time: 0 ms (100%), Space: 55.6 MB (17.76%) - LeetHub

--- 2379-minimum-recolors-to-get-k-consecutive-black-blocks/2379-minimum-recolors-to-get-k-consecutive-black-blocks.js
+++ 2379-minimum-recolors-to-get-k-consecutive-black-blocks/2379-minimum-recolors-to-get-k-consecutive-black-blocks.js
@@ -6,13 +6,20 @@
 var minimumRecolors = function(blocks, k) {
     let black = 0;
     let size = blocks.length
-
+    // WBBWWBBWBW"
+    // 0123456789
     for (let i = 0; i < blocks.length; i++) {
-        if (i - k >= 0 && blocks[i - k] === 'B') {
+        // we dont hit this condition until we've reached k len 
+        // when we do then we check that the first char is B so remove it from 
+        // the blacks estimates
+        const startChar = blocks[i - k]
+        const isValidLen = i - k >= 0
+        if (isValidLen && startChar === 'B') {
             black--
         }
         if (blocks[i] === "B") black++
-        size = Math.min(size, k - black)
+        const minWhites = k - black
+        size = Math.min(size, minWhites)
     }
     return size
 };
