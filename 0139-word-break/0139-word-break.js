e22f48433edf57ee816e995c5ca09fa3a5f809eb
Roger Perez <rogerprz@github.com>
2025-03-09 13:18:48 -0700

    Time: 4 ms (79.19%), Space: 55 MB (29.62%) - LeetHub

--- 0139-word-break/0139-word-break.js
+++ 0139-word-break/0139-word-break.js
@@ -9,19 +9,13 @@ var wordBreak = function(s, wordDict) {
     for (let i = 0; i < s.length;i++) {
         for (const word of wordDict) {
             const size = word.length;
-            // s = "leetcode", 
-                    01234567
-            // wordDict = ["leet","code"]
-            // 
             if (i === size - 1 || dp[i - size]){
                 const subString = s.substring(i - size + 1, i + 1)
-                console.log("S:", subString)
                 if (subString === word) {
                     dp[i] = true
                 }
             }
         }
     }
-    console.log('DP:', dp)
     return dp[dp.length-1]
 };
