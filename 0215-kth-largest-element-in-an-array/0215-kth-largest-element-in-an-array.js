458b5257f6b41af8554afb894fd610c186d95d7f
Roger Perez <rogerprz@github.com>
2025-03-09 13:39:32 -0700

    Time: 25 ms (85.46%), Space: 66.1 MB (50.6%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.js
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.js
@@ -4,28 +4,25 @@
  * @return {number}
  */
 var findKthLargest = function(nums, k) {
-    const min = Math.min(...nums)
     const max = Math.max(...nums)
-    // 1 
-    // 6
-    // [3,2,1,5,6,4]        7 + 1
-    const freq = new Array(max - min + 1).fill(0)
+    const min = Math.min(...nums)
 
-    for (let i = 0; i < nums.length;i++) {
-        const num = nums[i]
-        const total = num  - min
-        freq[total]++
+    const array = Array.from({ length: max - min + 1 }, ()=> 0)
+    console.log("M:", min, max, array)
+    for (const num of nums) {
+        const idx = num - min
+        array[idx]++
     }
-    console.log("F:", freq)
-    for (let i = freq.length - 1; i >=0;i--) {
-        const count = freq[i]
-        if (count === 0) continue 
-        // [1, 1, 1, 1, 1, 1, 0]
-        //  0 ,1, 2, 3, 4, 5, 6
-        k -= count
+    console.log("A:", array)
+    for (let i = array.length - 1; i >= 0; i--) {
+        if (array[i] === 0) continue
+
+        k -= array[i]
+        array[i] = 0 
 
         if (k <= 0) {
             return i + min
         }
     }
+    return 0
 };
