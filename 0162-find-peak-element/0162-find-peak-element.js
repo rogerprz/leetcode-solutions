a7f9ea4b56d04c0ba636c22c3dae56b3b3fc6da9
Roger Perez <rogerprz@github.com>
2025-03-09 13:52:32 -0700

    Time: 0 ms (100%), Space: 54 MB (9.91%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.js
+++ 0162-find-peak-element/0162-find-peak-element.js
@@ -3,18 +3,18 @@
  * @return {number}
  */
 var findPeakElement = function(nums) {
-    
-    const findPeak = (nums, l, r) =>{
-        if (l === r) {
-            return l
-        }
-        const mid = Math.floor((l+r)/2)
+    let left = 0;
+    let right = nums.length - 1
 
-        if (nums[mid] > nums[mid+1]) {
-            return findPeak(nums, l, mid)
+    while(left < right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if(nums[mid] < nums[mid + 1]) {
+            left = mid + 1
+        } else {
+            right = mid
         }
-        return findPeak(nums,mid+1, r)
     }
 
-    return findPeak(nums, 0, nums.length-1)
+    return left;
 };
