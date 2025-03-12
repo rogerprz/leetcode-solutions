99f966f4b086f4bcf4a20695d3ce56148c7cbeb2
Roger Perez <rogerprz@github.com>
2025-03-11 21:11:16 -0700

    Time: 0 ms (100%), Space: 54.9 MB (23.49%) - LeetHub

--- 2529-maximum-count-of-positive-integer-and-negative-integer/2529-maximum-count-of-positive-integer-and-negative-integer.js
+++ 2529-maximum-count-of-positive-integer-and-negative-integer/2529-maximum-count-of-positive-integer-and-negative-integer.js
@@ -3,15 +3,23 @@
  * @return {number}
  */
 var maximumCount = function(nums) {
-    let pos = 0;
-    let neg = 0
+    let negCount = binarySearch(nums, 0);
+    let posCount = nums.length - binarySearch(nums, 1);
+    return Math.max(negCount, posCount);
+};
 
-    for (const num of nums) {
-        if (num > 0) {
-            pos++
-        } else if (num < 0) {
-            neg++
+var binarySearch = function(nums, target) {
+    let left = 0, right = nums.length - 1, result = nums.length;
+    
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            result = mid;
+            right = mid - 1;
         }
     }
-    return Math.max(pos, neg)
+    
+    return result;
 };
