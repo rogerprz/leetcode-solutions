23281de91cf02cb0e03feb33847ae4a670673707
Roger Perez <rogerprz@github.com>
2025-03-12 08:21:33 -0700

    Time: 1 ms (79.74%), Space: 60 MB (9.8%) - LeetHub

--- 0545-boundary-of-binary-tree/0545-boundary-of-binary-tree.js
+++ 0545-boundary-of-binary-tree/0545-boundary-of-binary-tree.js
@@ -24,7 +24,7 @@ var boundaryOfBinaryTree = function(root)  {
     }
 
     const bottomBorder = (node) => {
-        if (!node) return;
+        if (!node) return node;
 
         if (!node.left && !node.right) {
             result.push(node.val)
@@ -52,44 +52,3 @@ var boundaryOfBinaryTree = function(root)  {
 
     return result 
 };
-
-/**
-if (!root) return null
-    const result = []
-
-    const leftBorder = (node) => {
-        while (node) {
-            if (node.left || node.right) result.push(node.val) 
-            node = node.left || node.right
-        }
-    }
-    const bottomBorder = (node) => {
-    if (!node) return 
-    if (!node.left && !node.right) { 
-        result.push(node.val) 
-        return
-        }
-    bottomBorder(node.left) 
-    bottomBorder(node.right) 
-    }
-
-    const rightBorder = (node) => {
-        const stack = []
-        while (node) {
-            if (node.left || node.right) stack.push(node.val) 
-            node = node.right || node.left
-        }
-        while (stack.length > 0) {
-            result.push(stack.pop() ) 
-        }
-    }
-
-
-    if (root.left || root.right) result.push(root.val) 
-
-    leftBorder(root.left) 
-    bottomBorder(root) 
-    rightBorder(root.right) 
-
-    return result
-     */
