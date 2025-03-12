fce54c377783bacc0bf47ee00b47b444b3a23abe
Roger Perez <rogerprz@github.com>
2025-03-11 21:38:43 -0700

    Time: 12 ms (94.69%), Space: 62.6 MB (27.44%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.js
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.js
@@ -4,6 +4,23 @@
  * @return {number}
  */
 var subarraySum = function(nums, k) {
+    const map = new Map()
+    let count = 0, sum = 0;
+
+    map.set(0,1)
+    for (let i = 0; i < nums.length;i++) {
+        sum += nums[i]
+
+        if (map.has(sum - k)) {
+            count += map.get(sum - k)
+        }
+        if (map.has(sum)) map.set(sum, map.get(sum)+1);
+        else map.set(sum, 1);
+    }
+    return count;
+};
+/**
+var subarraySum = function(nums, k) {
     let subNum = { 0: 1 };
     let total = 0, count = 0;
 
@@ -19,3 +36,4 @@ var subarraySum = function(nums, k) {
 
     return count;    
 };
+ */
