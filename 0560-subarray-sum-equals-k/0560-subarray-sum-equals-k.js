e98ac5cce7b12b857076a8267531d14dab7f936a
Roger Perez <rogerprz@github.com>
2025-03-12 10:44:15 -0700

    Time: 16 ms (78.46%), Space: 64.4 MB (14.4%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.js
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.js
@@ -4,7 +4,30 @@
  * @return {number}
  */
 var subarraySum = function(nums, k) {
-    const map = new Map()
+   let count = 0
+   let map = new Map()
+   let sum = 0;
+
+    map.set(0,1)
+    for (let i = 0; i < nums.length;i++) {
+        sum += nums[i]
+
+        if (map.get(sum-k)) {
+            count += map.get(sum - k)
+        }
+
+        map.set(sum, (map.get(sum) || 0) + 1)
+    }
+    return count
+};
+
+
+
+/**
+
+
+
+ const map = new Map()
     let count = 0, sum = 0;
 
     map.set(0,1)
@@ -15,26 +38,10 @@ var subarraySum = function(nums, k) {
             count += map.get(sum - k)
         }
 
-        if (map.has(sum)) map.set(sum, (map.get(sum))+1);
+        if (map.has(sum)) {
+            map.set(sum, (map.get(sum))+1);
+        }
         else map.set(sum, 1);
     }
     return count;
-};
-/**
-var subarraySum = function(nums, k) {
-    let subNum = { 0: 1 };
-    let total = 0, count = 0;
-
-    for (const n of nums) {
-        total += n;
-
-        if (subNum[total - k] !== undefined) {
-            count += subNum[total - k];
-        }
-
-        subNum[total] = (subNum[total] || 0) + 1;
-    }
-
-    return count;    
-};
  */
