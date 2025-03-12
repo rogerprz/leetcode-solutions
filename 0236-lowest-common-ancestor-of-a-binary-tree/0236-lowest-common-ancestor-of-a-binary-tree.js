beba4e9877147b51034d607dac75602eaa4c91ac
Roger Perez <rogerprz@github.com>
2025-03-11 22:48:39 -0700

    Time: 53 ms (91.04%), Space: 65.2 MB (11.89%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.js
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.js
@@ -19,11 +19,9 @@ var lowestCommonAncestor = function(root, p, q) {
     }
 
     const left = lowestCommonAncestor(root.left, p, q)
-    const right = lowestCommonAncestor(root.right, p,q)
+    const right = lowestCommonAncestor(root.right, p, q)
 
-    if (left && right) {
-        return root
-    }
+    if (left && right) return root;
 
     return left || right
 };
