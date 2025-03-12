9393c095f271d2d0ffd41eeb5f536bddb7e0b071
Roger Perez <rogerprz@github.com>
2025-03-11 22:21:29 -0700

    Time: 0 ms (100%), Space: 53.1 MB (31.34%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.js
+++ 0670-maximum-swap/0670-maximum-swap.js
@@ -3,31 +3,23 @@
  * @return {number}
  */
 var maximumSwap = function(num) {
-    // Convert the number to a string array
-    let nums = num.toString().split('');
-    
-    // Track the last occurrence of each digit
-    let last = new Array(10).fill(-1);
-    for (let i = 0; i < nums.length; i++) {
-        const num = parseInt(nums[i])
-        last[num] = i;
+    const nums = num.toString().split("")
+
+    const digits = new Array(10).fill(-1)
+    for (let i = 0; i < nums.length;i++) {
+        const n = parseInt(nums[i])
+        digits[n] = i
     }
 
-    // Traverse the digits from left to right
     for (let i = 0; i < nums.length; i++) {
-        // Check if a larger digit can be swapped later
         for (let d = 9; d > nums[i]; d--) {
-            const largestNum = last[d]
-            // we check that the idx/location of the largest
-            // can we swapped with current location
-            if (largestNum > i) {
-                // Swap the digits and return the new number
-                [nums[i], nums[largestNum]] = [nums[largestNum], nums[i]];
-                return parseInt(nums.join(''));
+            const numFromEnd = digits[d]
+
+            if (numFromEnd > i) {
+                [nums[i], nums[numFromEnd]] = [nums[numFromEnd], nums[i]]
+                return parseInt(nums.join(""))
             }
         }
     }
-    
-    // Return the original number if no swap was performed
-    return num;
+    return num
 };
