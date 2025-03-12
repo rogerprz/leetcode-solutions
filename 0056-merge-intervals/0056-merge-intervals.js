b284bdef0c7a484aff0c3769559c5abe1c778acf
Roger Perez <rogerprz@github.com>
2025-03-12 14:48:26 -0700

    Time: 13 ms (30.61%), Space: 65.2 MB (18.16%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.js
+++ 0056-merge-intervals/0056-merge-intervals.js
@@ -12,12 +12,12 @@ var merge = function(intervals) {
 
     // [[1,3],[2,6],
     for (let i = 1; i<intervals.length;i++) {
-        const curr = intervals[i]
-        if (curr[0] <= prev[1]) {
-            prev[1] = Math.max(prev[1], curr[1])
+        const [start, end] = intervals[i]
+        if (start <= prev[1]) {
+            prev[1] = Math.max(prev[1], end)
         } else {
-            res.push(curr)
-            prev = curr
+            res.push(intervals[i])
+            prev = intervals[i]
         }
     }
     return res
