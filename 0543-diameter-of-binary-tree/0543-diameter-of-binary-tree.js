b97266e626515f150da4b7d6f84ee34fdb61c80c
Roger Perez <rogerprz@github.com>
2025-03-12 10:45:48 -0700

    Time: 2 ms (39.56%), Space: 62.7 MB (9.39%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.js
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.js
@@ -11,19 +11,18 @@
  * @return {number}
  */
 var diameterOfBinaryTree = function(root) {
-    let max = 0;
+    let max = 0
 
     const dfs = (node) => {
-        if (!node)  return node
+        if (!node) return 0
 
-        const left = dfs(node.left);
-        const right = dfs(node.right);
+        const left = dfs(node.left)
+        const right = dfs(node.right)
 
         max = Math.max(max, left + right)
 
         return 1 + Math.max(left, right)
     }
-
     dfs(root)
     return max
 };
