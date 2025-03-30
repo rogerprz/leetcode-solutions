44d1133e75e3f965f02f0cd568acba3373ff38fe
Roger Perez <rogerprz@github.com>
2025-03-30 16:07:48 -0700

    Time: 5 ms (69.84%), Space: 55.2 MB (40.37%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.js
+++ 0763-partition-labels/0763-partition-labels.js
@@ -6,9 +6,10 @@ var partitionLabels = function(s) {
     let last = -1;
     const res = [];
     let left = 0;
-    
+    //  "ababcc" can be partitioned into ["abab", "cc"]
     for (let i = 0; i < s.length; i++) {
-        last = Math.max(s.lastIndexOf(s[i]), last)
+        const lastIdx = s.lastIndexOf(s[i])
+        last = Math.max(lastIdx, last)
 
         if (i === last) {
             res.push(i - left + 1)
