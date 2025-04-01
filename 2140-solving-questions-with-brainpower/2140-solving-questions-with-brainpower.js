58b08eb3deff211a6d05d45ac4cf6b12561d970f
Roger Perez <rogerprz@github.com>
2025-03-31 23:28:16 -0700

    Time: 14 ms (80.77%), Space: 90 MB (78.85%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.js
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.js
@@ -12,13 +12,19 @@ var mostPoints = function(questions) {
     for (let i = N - 1; i >= 0;i--) {
         const [points, skip] = questions[i]
 
+        // Option 1: Skip this question
         const skipPoints = dp[i + 1];
-    
+        
+        // Option 2: Solve this question
+        // If solving puts us beyond the array, we just get the points
+        // Otherwise, we add the maximum possible points from where we can resume
         const nextQuestion = i + skip + 1;
         const solvePoints = points + (nextQuestion < N ? dp[nextQuestion] : 0);
         
         // Take the maximum of the two options
         dp[i] = Math.max(skipPoints, solvePoints);
     }
-    return dp[0]
+    
+    // The maximum points possible starting from the first question
+    return dp[0];
 };
