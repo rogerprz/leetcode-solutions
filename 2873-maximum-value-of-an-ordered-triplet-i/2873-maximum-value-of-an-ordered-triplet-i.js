d323e6974a93bf71fe5fe6b644ebe6e5d11c260a
Roger Perez <rogerprz@github.com>
2025-04-01 22:39:29 -0700

    Time: 3 ms (29.73%), Space: 53.8 MB (78.38%) - LeetHub

--- 2873-maximum-value-of-an-ordered-triplet-i/2873-maximum-value-of-an-ordered-triplet-i.js
+++ 2873-maximum-value-of-an-ordered-triplet-i/2873-maximum-value-of-an-ordered-triplet-i.js
@@ -3,19 +3,8 @@
  * @return {number}
  */
 var maximumTripletValue = function(nums) {
-    // The value of a triplet of indices 
-    // (i, j, k) === (nums[i] - nums[j]) * nums[k]
-    // [12,6,1,2,7]
-    // [ 0,1,2,3,4]
-    //   i,j,    k
-    //  (   12 - 1)        * 7
-    // (nums[i] - nums[j]) * nums[k]
-
-    let p1 = 0;
-    let p3 = 2;
     let max = 0;
-    let diff = 0;
-
+    
     for (let i = 0; i < nums.length;i++) {
         for (let j = i +1; j < nums.length; j++) {
             for (let k = j + 1; k < nums.length; k++) {
