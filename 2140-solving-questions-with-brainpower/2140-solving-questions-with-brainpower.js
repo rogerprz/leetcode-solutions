3689b39b0c3383a03185944613aa3e90502f6edb
Roger Perez <rogerprz@github.com>
2025-04-02 08:52:43 -0700

    Time: 23 ms (68.63%), Space: 90.4 MB (78.43%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.js
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.js
@@ -4,26 +4,19 @@
  */
 var mostPoints = function(questions) {
     const dp = new Array(questions.length + 1).fill(0)
-    //  [0, 4, 4, 2, 4]
-    //   0,1,2,3
-    // [[3,2],[4,3],[4,4],[2,5]]
     const N = questions.length;
-
-    for (let i = N - 1; i >= 0;i--) {
+    //       0,    1,    2,    3,    4
+    //   [[1,1],[2,2],[3,3],[4,4],[5,5]]
+    // dp = [0,    0,    0,    0,    0,  0]
+    for (let i = N - 1; i >= 0; i--) {
         const [points, skip] = questions[i]
 
-        // Option 1: Skip this question
-        const skipPoints = dp[i + 1];
-        // Option 2: Solve this question
-        // If solving puts us beyond the array, we just get the points
-        // Otherwise, we add the maximum possible points from where we can resume
-        const prevQuestion = i + skip + 1;
-        const prevPoints = (prevQuestion < N ? dp[prevQuestion] : 0)
-        const solvePoints = points + prevPoints;
-        
-        // Take the maximum of the two options
-        dp[i] = Math.max(skipPoints, solvePoints);
+        const skipped = dp[i + 1];
+        const prev = i + skip + 1
+
+        const prevPoints = prev < N ? dp[prev] : 0
+        const totalPoints = points + prevPoints
+        dp[i] = Math.max(skipped, totalPoints)
     }
-    
-    return dp[0];
+    return dp[0]
 };
