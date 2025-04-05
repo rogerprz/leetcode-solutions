58d447caed70c3f0b44118f07e9640f94bf07702
Roger Perez <rogerprz@github.com>
2025-04-05 11:18:28 -0700

    Time: 7 ms (84.89%), Space: 65.1 MB (38.22%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.js
+++ 0056-merge-intervals/0056-merge-intervals.js
@@ -3,21 +3,21 @@
  * @return {number[][]}
  */
 var merge = function(intervals) {
-    // Input: [[1,3],[2,6],[8,10],[15,18]]
-    // Output: [[1,6],[8,10],[15,18]]
     intervals.sort((a,b) => a[0] - b[0])
-    let prev = intervals[0] // [1,3]
-    let res = [prev]
-
+    let prev = intervals[0]
+    const res = [prev]
+    // [[1,3],[2,6],[8,10],[15,18]]
+    //   ps      pe  s e  
+    // prev = [8,10]
+    // res = [[1,6], [8, 10]]
     for (let i = 1; i < intervals.length; i++) {
-        const curr = intervals[i]
-        const [start, end] = curr
-        
-        if (prev[1] >= start) {
-            prev[1] = Math.max(prev[1], end)
+        const [start, end] = intervals[i]
+
+        if (start <= prev[1]) {
+            prev[1] = Math.max(end, prev[1])
         } else {
-            res.push(curr)
-            prev = curr
+            res.push(intervals[i])
+            prev = intervals[i]
         }
     }
     return res
