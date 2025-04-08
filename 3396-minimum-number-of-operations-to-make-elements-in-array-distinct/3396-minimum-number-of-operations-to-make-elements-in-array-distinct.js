b99acbad04e1a0fbe7c097ba871e8bb87c083ad0
Roger Perez <rogerprz@github.com>
2025-04-07 21:11:09 -0700

    Time: 2 ms (88.51%), Space: 55.9 MB (58.62%) - LeetHub

--- 3396-minimum-number-of-operations-to-make-elements-in-array-distinct/3396-minimum-number-of-operations-to-make-elements-in-array-distinct.js
+++ 3396-minimum-number-of-operations-to-make-elements-in-array-distinct/3396-minimum-number-of-operations-to-make-elements-in-array-distinct.js
@@ -4,14 +4,8 @@
  */
 var minimumOperations = function(nums) {
     const set = new Set()
-
     let index = 0
-    // set = 7, 5, 3 
-    //  [1,2,3,4,2,3,3,5,7]
-    //  [4,5,6,4,4]
-    //  [6,7,8,9]
-    //.  0,1,2,3,4,5,6,7,8
-    //             i
+
     for (let i = nums.length - 1; i >= 0; i--) {
         const num = nums[i]
         if (set.has(num)) {
@@ -20,8 +14,7 @@ var minimumOperations = function(nums) {
         }
         set.add(num)
     }
-    // 6 / 3 = 2 
-    // 4/ 3 = 2
+
     if (index === 0) {
         return index
     }
