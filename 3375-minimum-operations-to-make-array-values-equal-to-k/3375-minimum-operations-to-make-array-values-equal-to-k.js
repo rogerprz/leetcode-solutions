4fabaad18c2822df7f1024755990294e9113bfa2
Roger Perez <rogerprz@github.com>
2025-04-08 20:53:46 -0700

    Time: 65 ms (30.61%), Space: 57.4 MB (26.53%) - LeetHub

---
+++ 3375-minimum-operations-to-make-array-values-equal-to-k/3375-minimum-operations-to-make-array-values-equal-to-k.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var minOperations = function(nums, k) {
+    const set = new Set();
+    for (const x of nums) {
+        if (x < k) {
+            return -1;
+        } else if (x > k) {
+            set.add(x);
+        }
+    }
+    return set.size;
+};
