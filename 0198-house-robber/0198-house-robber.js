36b79667a42bdb62e6088f4df89c380a15e819f9
Roger Perez <rogerprz@github.com>
2025-04-08 22:14:46 -0700

    Time: 0 ms (100%), Space: 53.5 MB (49.93%) - LeetHub

--- 0198-house-robber/0198-house-robber.js
+++ 0198-house-robber/0198-house-robber.js
@@ -2,8 +2,29 @@
  * @param {number[]} nums
  * @return {number}
  */
-var rob = function(nums, index = 0) {
-    let memo = []
+var rob = function(nums) {
+  const memo = [];
+
+  const dp = (i) => {
+    if (i >= nums.length) {
+        return 0
+    }
+    if (memo[i] != null) {
+        return memo[i]
+    }
+    const next = dp(i + 1)
+    const oneAfter = dp (i + 2)
+    memo[i] = Math.max(nums[i] + oneAfter, next)
+
+    return memo[i]
+  } 
+
+  return dp(0) 
+};
+
+
+/**
+ let memo = []
     const dp = (i) => {
         if (i >=nums.length) {
             return 0
@@ -17,4 +38,4 @@ var rob = function(nums, index = 0) {
         return memo[i]
     }
     return dp(0)
-};
+     */
