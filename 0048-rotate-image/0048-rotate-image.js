082dde3ba712e3ba7f5242f94265290a8dd3dba7
Roger Perez <rogerprz@github.com>
2025-04-10 20:53:56 -0700

    Time: 0 ms (100%), Space: 53.8 MB (71.95%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.js
+++ 0048-rotate-image/0048-rotate-image.js
@@ -5,15 +5,13 @@
 var rotate = function(matrix) {
     if (matrix.length <= 1) return matrix 
     const n = matrix.length;
-    
-    for (var i = 0; i < Math.floor((n + 1) / 2); i++) {
-        for (var j = 0; j < Math.floor(n / 2); j++) {
-            var temp = matrix[n - 1 - j][i];
-            matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1];
-            matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 - i];
-            matrix[j][n - 1 - i] = matrix[i][j];
-            matrix[i][j] = temp;
+
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = i + 1; j < matrix.length; j++) {
+            [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
         }
     }
+
+    matrix.forEach(row => row.reverse());
     return matrix
 };
