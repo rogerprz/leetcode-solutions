27049241d06fd8c7231734f8cf0cf9811962f136
Roger Perez <rogerprz@github.com>
2025-04-17 13:30:29 -0700

    Time: 28 ms (68.97%), Space: 97.9 MB (17.24%) - LeetHub

--- 2672-number-of-adjacent-elements-with-the-same-color/2672-number-of-adjacent-elements-with-the-same-color.js
+++ 2672-number-of-adjacent-elements-with-the-same-color/2672-number-of-adjacent-elements-with-the-same-color.js
@@ -10,17 +10,16 @@ var colorTheArray = function(n, queries) {
 
     for (let i = 0; i < queries.length; i++) {
         const [currIdx, color] = queries[i]
+        const prevIdx = currIdx - 1;
+        const nextIdx = currIdx + 1;
         // Remove old pairs if not zero
         if (colors[currIdx] !== 0) {
             // was uncolored, cant have been a pair
             // was a color, could be pair
-            const prevIdx = currIdx - 1;
-            const nextIdx = currIdx + 1;
-            // edge case to be >= 0 
+
             if (prevIdx >= 0 &&  colors[currIdx] == colors[prevIdx]) {
                 pairs--
             }
-            // edge case to < colors.length;
             if (nextIdx < colors.length &&  colors[currIdx] == colors[nextIdx]) {
                 pairs--
             } 
@@ -28,10 +27,10 @@ var colorTheArray = function(n, queries) {
 
         colors[currIdx] = color
 
-        if (currIdx - 1 >= 0 &&  colors[currIdx] == colors[currIdx-1]) {
+        if (prevIdx >= 0 &&  colors[currIdx] == colors[prevIdx]) {
             pairs++
         }
-        if (currIdx + 1 < colors.length &&  colors[currIdx] == colors[currIdx+1]) {
+        if (nextIdx < colors.length &&  colors[currIdx] == colors[nextIdx]) {
             pairs++
         } 
 
