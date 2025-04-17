1e7adff19620ea6767b6ee11a4d5ed1c2f25ec0c
Roger Perez <rogerprz@github.com>
2025-04-16 23:12:21 -0700

    Time: 7 ms (85.71%), Space: 61 MB (59.23%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.js
@@ -0,0 +1,40 @@
+/**
+ * @param {character[]} tasks
+ * @param {number} n
+ * @return {number}
+ */
+var leastInterval = function(tasks, n) {
+    const freq = Array(26).fill(0)
+    let N = tasks.length;
+    for (const task of tasks) {
+        freq[task.charCodeAt(0) - "A".charCodeAt(0)]++
+    }
+
+    freq.sort((a,b) => b-a)
+
+    let chunk = freq[0] - 1;
+    let idle = chunk * n;
+
+    for (let i = 1; i < 26; i++) {
+        idle -= Math.min(chunk, freq[i]);
+    }
+
+    return idle < 0 ? N  : N  + idle;
+};
+
+/**
+
+    for (const task of tasks) {
+        freq[task.charCodeAt(0) - 'A'.charCodeAt(0)]++;
+    }
+
+    freq.sort((a,b)=> b-a)
+    let chunk = freq[0] - 1;
+    let idle = chunk * n;
+
+    for (let i = 1; i < 26; i++) {
+        idle -= Math.min(chunk, freq[i])
+    }
+
+    return idle < 0 ? tasks.length : tasks.length + idle
+     */
