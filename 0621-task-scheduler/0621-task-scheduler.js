1490edc668937c60877c56a97bec9d658bfff407
Roger Perez <rogerprz@github.com>
2025-04-17 13:52:47 -0700

    Time: 5 ms (95.31%), Space: 61.2 MB (58.33%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.js
+++ 0621-task-scheduler/0621-task-scheduler.js
@@ -4,38 +4,20 @@
  * @return {number}
  */
 var leastInterval = function(tasks, n) {
-    const freq = Array(26).fill(0)
-    let N = tasks.length;
-    for (const task of tasks) {
-        freq[task.charCodeAt(0) - "A".charCodeAt(0)]++
+    const N = tasks.length;
+
+    const freq = new Array(26).fill(0)
+
+    for (const char of tasks) {
+        freq[char.charCodeAt(0) - "A".charCodeAt(0)]++
     }
-
-    freq.sort((a,b) => b-a)
-
-    let chunk = freq[0] - 1;
-    let idle = chunk * n;
-
-    for (let i = 1; i < 26; i++) {
-        if (freq[i] === 0) break;
-        idle -= Math.min(chunk, freq[i]);
-    }
-
-    return idle < 0 ? N  : N  + idle;
-};
-
-/**
-
-    for (const task of tasks) {
-        freq[task.charCodeAt(0) - 'A'.charCodeAt(0)]++;
-    }
-
-    freq.sort((a,b)=> b-a)
-    let chunk = freq[0] - 1;
-    let idle = chunk * n;
+    freq.sort((a,b) => b - a)
+    const chunk = freq[0] - 1
+    let idle = chunk * n 
 
     for (let i = 1; i < 26; i++) {
         idle -= Math.min(chunk, freq[i])
     }
 
-    return idle < 0 ? tasks.length : tasks.length + idle
-     */
+    return idle < 0 ? N : N + idle
+};
