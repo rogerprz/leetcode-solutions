67fc150478003029c2bfe675d706538e85253ff8
Roger Perez <rogerprz@github.com>
2025-04-20 20:53:42 -0700

    Time: 34 ms (90.08%), Space: 54.5 MB (39.26%) - LeetHub

--- 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.js
+++ 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.js
@@ -13,35 +13,26 @@
  * @return {_Node}
  */
 var flatten = function(head) {
-   if (!head) return null;
-   let node = head;
-   const stack = [];
+    let node = head;
+    const stack = [];
 
     while (node) {
-        // if a child, we need to push next
         if (node.child) {
-            // send next to stack while we re-assign next
             if (node.next) stack.push(node.next)
-            // assign next to the child 
+
             node.next = node.child;
-            // assign child prev to the curr node,
-            // bc child prev is null;
-            node.child.prev = node;
-            // remove the child by null;
+            node.child.prev = node
             node.child = null;
         }
-        // if no node.next & stack.len > 0
-        // we need grab from stack to continue flatten
+
+
         if (!node.next && stack.length > 0) {
-            // get prev node level list
             let nextNode = stack.pop();
-            // assign next null to popped node
+
             node.next = nextNode;
-            // let nextNode know it's prev is node
             nextNode.prev = node
         }
-        node = node.next;
+        node = node.next
     }
-
-    return head
+    return head;
 };
