53fb472e6fc3a57f1ba16013d6b8a92cb5d725e1
Roger Perez <rogerprz@github.com>
2025-04-26 21:23:52 -0700

    Time: 0 ms (100%), Space: 53.3 MB (64.2%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.js
+++ 0054-spiral-matrix/0054-spiral-matrix.js
@@ -3,44 +3,45 @@
  * @return {number[]}
  */
 var spiralOrder = function(matrix) {
-    const res = []
+    const res = [];
+
     if (matrix.length === 0) return res;
 
-    let top = 0, left = 0;
+    let top = 0;
+    let left = 0;
     let bot = matrix.length - 1;
     let right = matrix[0].length - 1;
 
-
     while (top <= bot && left <= right) {
-        // traverse l to r 
-        for (let i = left; i <= right; i++) {
+        /**
+         0,1,2
+        [1,2,3], 0
+        [4,5,6], 1 
+        [7,8,9], 2
+         */
+        for (let i = left; i <= right;i++) {
             res.push(matrix[top][i])
         }
         top++
-        // traverse top to bot 
-        for (let i = top; i <= bot; i++) {
+        for (let i = top; i <= bot;i++) {
             res.push(matrix[i][right])
         }
         right--
-        // Check if we still have rows and columns to process
+        
         if (top <= bot) {
-            for (let i = right; i >= left; i--) {
+            for (let i = right; i >= left;i--) {
                 res.push(matrix[bot][i])
             }
             bot--
         }
-        // 3. Traverse left (bottom row)
 
         if (left <= right) {
-            for (let i = bot; i >= top; i--) {
+            for (let i = bot; i >= top;i--) {
                 res.push(matrix[i][left])
             }
             left++
         }
-        // 4. Traverse up (leftmost column)
+
     }
-
-    
-
     return res;
 };
