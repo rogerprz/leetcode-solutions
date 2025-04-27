3c53ad2a3fc398faafc73558edd9c3fb9358ab53
Roger Perez <rogerprz@github.com>
2025-04-27 14:15:50 -0700

    Time: 2 ms (45.25%), Space: 56.8 MB (39.8%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.js
+++ 0042-trapping-rain-water/0042-trapping-rain-water.js
@@ -5,22 +5,22 @@
 var trap = function(height) {
     let l = 0;
     let r = height.length - 1;
+
     let leftMax = height[0];
     let rightMax = height[r];
     let sum = 0;
+
     while (l < r){
-        if(leftMax <= rightMax){
+        if (leftMax < rightMax){
             sum += leftMax - height[l];
             l++;
            leftMax = Math.max(leftMax, height[l]);
 
-        }else{
-            sum += (rightMax - height[r] );
+        } else{
+            sum += rightMax - height[r];
             r--;
             rightMax = Math.max(rightMax, height[r])
         }
     }
     return sum
 };
-// ALT SOLUTIONS
-// https://leetcode.com/problems/trapping-rain-water/solutions/400555/clean-javascript-solutions-brute-force-dynamic-programming-stack-two-pointers/?envType=daily-question&envId=2024-04-12
