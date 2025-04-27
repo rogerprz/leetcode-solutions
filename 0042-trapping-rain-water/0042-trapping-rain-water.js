99cac430557f4f2ec0530dbf33dbd398a513ba70
Roger Perez <rogerprz@github.com>
2025-04-27 14:24:14 -0700

    Time: 0 ms (100%), Space: 54.8 MB (83.91%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.js
+++ 0042-trapping-rain-water/0042-trapping-rain-water.js
@@ -5,22 +5,20 @@
 var trap = function(height) {
     let l = 0;
     let r = height.length - 1;
-
-    let leftMax = height[0];
-    let rightMax = height[r];
+    let leftMax = height[l]
+    let rightMax = height[r]
     let sum = 0;
-
-    while (l < r){
-        if (leftMax < rightMax){
-            sum += leftMax - height[l];
+    while (l < r) {
+        if (leftMax <= rightMax) {
+            sum += leftMax - height[l]
             l++;
-           leftMax = Math.max(leftMax, height[l]);
-
-        } else{
-            sum += rightMax - height[r];
+            leftMax = Math.max(leftMax, height[l])
+        } else {
+            sum += rightMax - height[r]
             r--;
             rightMax = Math.max(rightMax, height[r])
         }
     }
-    return sum
+
+    return sum;
 };
