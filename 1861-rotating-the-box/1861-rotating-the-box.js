0eb287571d705c1234a3daf2ec30c089dcdfe3b5
Roger Perez <rogerprz@github.com>
2025-04-27 12:08:26 -0700

    Time: 26 ms (59.89%), Space: 83.3 MB (65.24%) - LeetHub

--- 1861-rotating-the-box/1861-rotating-the-box.js
+++ 1861-rotating-the-box/1861-rotating-the-box.js
@@ -2,42 +2,32 @@
  * @param {character[][]} boxGrid
  * @return {character[][]}
  */
-var rotateTheBox = function(box) {
-    const rows = box.length;
-    const cols = box[0].length;
-    const res = Array.from({length: cols}, ()=> 0)
-                    .map(()=> new Array(rows).fill('.'))
-    const STONE = "#";
-    const WALL = "*"
-    /**
-    [
-        ['#', '#', '*', '.', '*', '.'], 
-        ['#', '#', '#', '*', '.', '.'], 
-        ['#', '#', '#', '.', '#', '.']
-        ]
-    [
-        ['.', '.', '.'], 
-        ['.', '.', '.'], 
-        ['.', '.', '.'], 
-        ['.', '.', '.'], 
-        ['.', '.', '.'], 
-        ['.', '.', '.']
-        ]
-     */
-    for (let row = 0; row < rows;row++) {
-        let resColIdx = cols - 1;
+var rotateTheBox = function(boxGrid) {
+  let rows = boxGrid.length;
+  let cols = boxGrid[0].length;
 
-        for (let col = cols - 1; col >= 0; col--) {
-            const currPos = box[row][col]
-            const resRowIdx = rows - row - 1
-            if (currPos === STONE) {
-                res[resColIdx][resRowIdx] = STONE;
-                resColIdx--
-            } else if (currPos === WALL) {
-                res[col][resRowIdx] = WALL;
-                resColIdx = col - 1;
-            }
+  const res = Array.from({length: cols}, ()=> 0).map(()=>{
+    return new Array(rows).fill('.')
+  })
+  const STONE = '#';
+  const WALL = '*';
+
+  for (let row = 0; row < rows;row++) {
+    let resColIdx = cols - 1;
+
+    for (let col = cols - 1; col >= 0; col--) {
+        const curr = boxGrid[row][col]
+        let resRowIdx = rows - row - 1; 
+
+        if (curr === STONE) {
+            res[resColIdx][resRowIdx] = STONE;
+            resColIdx--
+        }
+        else if (curr === WALL) {
+            res[col][resRowIdx] = WALL;
+            resColIdx = col - 1; 
         }
     }
-    return res;
+  }
+  return res;
 };
