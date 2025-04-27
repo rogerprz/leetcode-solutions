e664b0d384bea92deee537998c5c8de63df6f32f
Roger Perez <rogerprz@github.com>
2025-04-27 15:16:47 -0700

    Time: 54 ms (22.22%), Space: 72.2 MB (88.89%) - LeetHub

--- 2365-task-scheduler-ii/2365-task-scheduler-ii.js
+++ 2365-task-scheduler-ii/2365-task-scheduler-ii.js
@@ -7,7 +7,7 @@ var taskSchedulerII = function(tasks, space) {
     const map = {}
     let days = 0;
     for (const task of tasks) {
-        if ((task in map)) {
+        if (task in map) {
             days = Math.max(days, map[task])
         }
         map[task] = days + space + 1;
