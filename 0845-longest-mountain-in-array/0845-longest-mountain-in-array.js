7c2dfe8ea7d477b71e46ed095c7f5b5676501bd5
Roger Perez <rogerprz@github.com>
2025-04-27 13:40:13 -0700

    Time: 1 ms (94.44%), Space: 55.3 MB (90.28%) - LeetHub

--- 0845-longest-mountain-in-array/0845-longest-mountain-in-array.js
+++ 0845-longest-mountain-in-array/0845-longest-mountain-in-array.js
@@ -4,27 +4,27 @@
  */
 var longestMountain = function(arr) {
     if (arr.length < 3) return 0;
-    // [2,1,4,7,3,2,5]
-    let base = 0;
-    let ans = 0; 
-    const N = arr.length - 1
-    
-    while (base < N) {
-        let end = base;
-        if (end + 1 < N && arr[end] < arr[end + 1]) {
-            while (end < N && arr[end] < arr[end + 1]) {
+    const N = arr.length -1;
+
+    let start= 0;
+    let res = 0; 
+
+    while (start < N) {
+        let end = start;
+
+        if (end < N && arr[end] < arr[end+1]) {
+            while (end < N && arr[end] < arr[end+1]) {
                 end++
             }
 
-            if ( end < N && arr[end] > arr[end + 1]) {
-                while (end < N && arr[end] > arr[end +1]) {
+            if (end < N && arr[end] > arr[end+1]) {
+                while (end < N && arr[end] > arr[end+1]) {
                     end++
                 }
-                ans = Math.max(ans, end - base + 1)
+                res = Math.max(res, end - start + 1) 
             }
         }
-        base = Math.max(end, base + 1)
+        start = Math.max(end, start + 1)
     }
-
-    return ans
+    return res;
 };
