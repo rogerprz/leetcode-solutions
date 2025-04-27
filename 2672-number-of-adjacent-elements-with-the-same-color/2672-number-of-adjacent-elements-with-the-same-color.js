698f22b2d246508292678b5cacb2068731e6c560
Roger Perez <rogerprz@github.com>
2025-04-26 22:02:21 -0700

    Time: 19 ms (93.33%), Space: 100.2 MB (8.89%) - LeetHub

--- 2672-number-of-adjacent-elements-with-the-same-color/2672-number-of-adjacent-elements-with-the-same-color.js
+++ 2672-number-of-adjacent-elements-with-the-same-color/2672-number-of-adjacent-elements-with-the-same-color.js
@@ -4,38 +4,33 @@
  * @return {number[]}
  */
 var colorTheArray = function(n, queries) {
-    let pairs = 0
-    let colors = new Array(n).fill(0)
-    let result = []
-
-    for (let i = 0; i < queries.length; i++) {
+    const colors = new Array(n).fill(0)
+    let pairs = 0;
+    const N = colors.length;
+    const res = []
+    for (let i = 0; i < queries.length;i++) {
         const [currIdx, color] = queries[i]
-        const prevIdx = currIdx - 1;
         const nextIdx = currIdx + 1;
-        // Remove old pairs if not zero
-        if (colors[currIdx] !== 0) {
-            // was uncolored, cant have been a pair
-            // was a color, could be pair
+        const prevIdx = currIdx - 1;
 
-            if (prevIdx >= 0 &&  colors[currIdx] == colors[prevIdx]) {
+        if (colors[currIdx] !== 0) {
+            if (prevIdx >= 0 && colors[currIdx] === colors[prevIdx]) {
                 pairs--
             }
-            if (nextIdx < colors.length &&  colors[currIdx] == colors[nextIdx]) {
+            if (nextIdx < N  && colors[currIdx] === colors[nextIdx]) {
                 pairs--
-            } 
+            }
         }
+        colors[currIdx] = color;
 
-        colors[currIdx] = color
-
-        if (prevIdx >= 0 &&  colors[currIdx] == colors[prevIdx]) {
+        if (prevIdx >= 0 && colors[currIdx] === colors[prevIdx]) {
             pairs++
         }
-        if (nextIdx < colors.length &&  colors[currIdx] == colors[nextIdx]) {
+        if (nextIdx < N && colors[currIdx] === colors[nextIdx]) {
             pairs++
-        } 
-
-        result.push(pairs)
+        }
+        res.push(pairs)
     }
 
-    return result
+    return res;
 };
