c34c7240f2bbb9f25d88311067d85a8164a23feb
Roger Perez <rogerprz@github.com>
2025-04-27 20:22:31 -0700

    Time: 31 ms (63.64%), Space: 95.5 MB (65.91%) - LeetHub

--- 2672-number-of-adjacent-elements-with-the-same-color/2672-number-of-adjacent-elements-with-the-same-color.js
+++ 2672-number-of-adjacent-elements-with-the-same-color/2672-number-of-adjacent-elements-with-the-same-color.js
@@ -4,23 +4,33 @@
  * @return {number[]}
  */
 var colorTheArray = function(n, queries) {
-    const colors = new Array(n).fill(0)
     let pairs = 0;
-    let res = []
+    let colors = new Array(n).fill(0)
+    let res = [];
+    const N = colors.length
     for (let i = 0; i < queries.length;i++) {
-        const [idx, color] = queries[i]
+        const [idx, color] = queries[i];
         const prevIdx = idx - 1;
         const nextIdx = idx + 1;
 
         if (colors[idx] !== 0) {
-            if (prevIdx >= 0 && colors[idx] === colors[prevIdx]) pairs--
-            if (nextIdx < colors.length && colors[idx] === colors[nextIdx]) pairs--
+            if (prevIdx >= 0 && colors[idx] === colors[prevIdx]) {
+                pairs--
+            }
+            if (nextIdx < N && colors[idx] === colors[nextIdx]) {
+                pairs--
+            }
         }
         colors[idx] = color
 
-        if (prevIdx >= 0 && colors[idx] === colors[prevIdx]) pairs++
-        if (nextIdx < colors.length && colors[idx] === colors[nextIdx]) pairs++
+        if (prevIdx >= 0 && colors[idx] === colors[prevIdx]) {
+            pairs++
+        }
+        if (nextIdx <= N && colors[idx] === colors[nextIdx]) {
+            pairs++
+        }
         res.push(pairs)
     }
-    return res
+
+    return res;
 };
