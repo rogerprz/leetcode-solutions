2478afddef1c6eff07f306b2bc6cd8db69ddfd16
Roger Perez <rogerprz@github.com>
2025-04-29 13:42:27 -0700

    Time: 2 ms (80.98%), Space: 63.6 MB (17.05%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.js
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -6,12 +6,11 @@ var maxArea = function(height) {
     let left = 0
     let right = height.length - 1
     let max = 0
-    let width = 0
     // [1,8,6,2,5,4,8,3,7] 9
     while (left < right) {
         const lHeight = height[left]
         const rHeight = height[right]
-        width = right - left
+        let width  = right - left
         const minHeight = Math.min(lHeight, rHeight)
 
         max = Math.max(max, minHeight * width)
