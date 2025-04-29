b3d723b5ca2cabbc6631f3d16fcfa66179cb9df4
Roger Perez <rogerprz@github.com>
2025-04-28 22:38:13 -0700

    Time: 18 ms (86.32%), Space: 84.2 MB (44.21%) - LeetHub

--- 1861-rotating-the-box/1861-rotating-the-box.js
+++ 1861-rotating-the-box/1861-rotating-the-box.js
@@ -2,52 +2,29 @@
  * @param {character[][]} boxGrid
  * @return {character[][]}
  */
-var rotateTheBox = function(box) {
-    const rows = box.length;
-    const cols = box[0].length;
+var rotateTheBox = function(boxGrid) {
+    let rows = boxGrid.length;
+    let cols = boxGrid[0].length;
 
-    const res = new Array(cols).fill(0).map(()=> new Array(rows).fill('.'))
+    let res = new Array(cols).fill(0).map(() => new Array(rows).fill("."))
     const STONE = "#";
-    const WALL = "*";
+    const WALL = "*"
 
-    console.log("R:", res);
-
-    for (let row = 0; row < rows;row++) {
+    for (let row = 0; row < rows; row++) {
         let resColIdx = cols - 1;
-        for (let col = cols-1; col >= 0; col--) {
+        for (let col = cols -1; col >= 0; col--) {
             let resRowIdx = rows - 1 - row;
-            const curr = box[row][col]
-            if (curr == STONE) {
-                res[resColIdx][resRowIdx] = curr;
+            const curr = boxGrid[row][col];
+            if (curr === STONE) {
+                res[resColIdx][resRowIdx]= curr;
                 resColIdx--
             }
-            else if (curr === WALL) {
+            if (curr === WALL) {
                 res[col][resRowIdx] = curr;
                 resColIdx = col - 1;
             }
         }
-       
     }
 
     return res;
 };
-
-/**
-[ 
-    [ '.' ], 
-    [ '.' ], 
-    [ '#' ] ]
- for (let col = cols - 1; col >= 0; col--){
-            let resRowsIdx = rows - row - 1;
-            const curr = box[row][col]
-
-            if (curr === STONE) {
-                res[resColIdx][resRowsIdx] = curr;
-                resColIdx--
-            } else if (curr === WALL) {
-                res[col][resRowsIdx] = curr;
-                resColIdx = col - 1;
-            }
-
-        }
-         */
