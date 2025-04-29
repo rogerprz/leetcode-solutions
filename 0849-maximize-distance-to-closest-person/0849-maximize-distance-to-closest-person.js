db58df0f2e26c703b230cf557933d7328603f1f9
Roger Perez <rogerprz@github.com>
2025-04-28 23:05:12 -0700

    Time: 4 ms (41.85%), Space: 55.3 MB (88.15%) - LeetHub

--- 0849-maximize-distance-to-closest-person/0849-maximize-distance-to-closest-person.js
+++ 0849-maximize-distance-to-closest-person/0849-maximize-distance-to-closest-person.js
@@ -4,27 +4,29 @@
  */
 var maxDistToClosest = function(seats) {
     let max = 0;
+
     let left = 0;
     const N = seats.length;
-
-    for (let i = 0; i < N; i++) {
-        const seat = seats[i]
+    for (let i = 0; i < N;i++) {
+        const seat = seats[i];
 
         if (seat === 1) {
+            // start
             if (seats[0] === 0 && left === 0) {
-                const diff = i - left;
-                max = Math.max(max, diff)
+                const diff = i - left
+                max = Math.max(diff, max)
             } else {
-                const midDiff = Math.floor((i - left)/2)
-                max = Math.max(max, midDiff)
+                // in between/mid
+                const diff = Math.floor((i - left)/2)
+                max = Math.max(diff, max)
             }
             left = i;
         }
-        if (seats[N-1] === 0 && i === N - 1) {
-            const diff = i - left;
-            max = Math.max(max, diff)
+        // end scenario
+        if (seats[N - 1] === 0 && i === N - 1) {
+                const diff = i - left;
+                max = Math.max(diff, max)
         }
     }
-
     return max;
 };
