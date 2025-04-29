fd4c7cf3f63ebcb07f6c1eeaac7b46b7cdc462ba
Roger Perez <rogerprz@github.com>
2025-04-28 21:55:10 -0700

    Time: 90 ms (20%), Space: 87 MB (8.42%) - LeetHub

--- 1861-rotating-the-box/1861-rotating-the-box.js
+++ 1861-rotating-the-box/1861-rotating-the-box.js
@@ -2,39 +2,52 @@
  * @param {character[][]} boxGrid
  * @return {character[][]}
  */
-var rotateTheBox = function(boxGrid) {
-  let rows = boxGrid.length;
-  let cols = boxGrid[0].length;
+var rotateTheBox = function(box) {
+    const rows = box.length;
+    const cols = box[0].length;
 
-    // create a tranposed array. 
-  const res = Array.from({length: cols}, ()=> 0).map(()=>{
-    return new Array(rows).fill('.')
-  })
-  const STONE = '#';
-  const WALL = '*';
+    const res = new Array(cols).fill(0).map(()=> new Array(rows).fill('.'))
+    const STONE = "#";
+    const WALL = "*";
 
-  for (let row = 0; row < rows;row++) {
-    // set col idx of the res array
-    let resColIdx = cols - 1;
-    // start col from end of grid going down
-    for (let col = cols - 1; col >= 0; col--) {
-        // get curr grid box 
-        const curr = boxGrid[row][col]
-        // start at end row - sub curr row - 1 for offset
-        let resRowIdx = rows - row - 1; 
-        // if we find stone
-        if (curr === STONE) {
-            // we set res box with flipped col idx and row idx
-            res[resColIdx][resRowIdx] = STONE;
-            // we sub resCol idx since it's been assign
-            resColIdx--
+    console.log("R:", res);
+
+    for (let row = 0; row < rows;row++) {
+        let resColIdx = cols - 1;
+        for (let col = cols-1; col >= 0; col--) {
+            let resRowIdx = rows - 1 - row;
+            const curr = box[row][col]
+            if (curr == STONE) {
+                res[resColIdx][resRowIdx] = curr;
+                resColIdx--
+            }
+            else if (curr === WALL) {
+                res[col][resRowIdx] = curr;
+                resColIdx = col - 1;
+            }
         }
-        else if (curr === WALL) {
-            
-            res[col][resRowIdx] = WALL;
-            resColIdx = col - 1; 
-        }
+       
     }
-  }
-  return res;
+
+    return res;
 };
+
+/**
+[ 
+    [ '.' ], 
+    [ '.' ], 
+    [ '#' ] ]
+ for (let col = cols - 1; col >= 0; col--){
+            let resRowsIdx = rows - row - 1;
+            const curr = box[row][col]
+
+            if (curr === STONE) {
+                res[resColIdx][resRowsIdx] = curr;
+                resColIdx--
+            } else if (curr === WALL) {
+                res[col][resRowsIdx] = curr;
+                resColIdx = col - 1;
+            }
+
+        }
+         */
