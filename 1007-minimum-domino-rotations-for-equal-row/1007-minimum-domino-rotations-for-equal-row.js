afd2e15db42562e7033b4d905d92f1d0bfe891aa
Roger Perez <rogerprz@github.com>
2025-05-03 12:09:23 -0700

    Time: 4 ms (70.83%), Space: 63.2 MB (85.42%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.js
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.js
@@ -4,7 +4,11 @@
  * @return {number}
  */
 var minDominoRotations = function(tops, bottoms) {
-    let top1 = tops[0], bottom1 = bottoms[0], ans = -1, topCount = 0, botCount = 0;
+    let top1 = tops[0]
+    let bottom1 = bottoms[0]
+    let ans = -1
+    let topCount = 0
+    let botCount = 0;
 
     for(let target of [top1, bottom1]){
         topCount = 0
