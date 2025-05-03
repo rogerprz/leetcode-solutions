3e861284d0603c174556d146d072930cc6fe5a62
Roger Perez <rogerprz@github.com>
2025-05-03 12:09:07 -0700

    Time: 2 ms (97.92%), Space: 64.4 MB (39.58%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.js
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.js
@@ -4,47 +4,22 @@
  * @return {number}
  */
 var minDominoRotations = function(tops, bottoms) {
-    // we need to keep track of each val
-    // we need to count to get a max len
-    let hash = {}
+    let top1 = tops[0], bottom1 = bottoms[0], ans = -1, topCount = 0, botCount = 0;
 
-    /**
-    2: [T,T,T,T,T,T]
-    5: [t,f,f,f,f,f]
-    1: [f,T,f,f,f,f]
-    6: [f,f,T,f,f,f]
-    4: [f,f,f,T,f,f]
-     */
-    const set = new Set()
-    for (let i = 0; i < tops.length; i++) {
-        const topVal = tops[i]
-        const botVal = bottoms[i]
+    for(let target of [top1, bottom1]){
+        topCount = 0
+        botCount = 0
+        for(let i=0; i<tops.length; i++){
+            if(tops[i] !== target && bottoms[i] !== target){
+                break
+            }
 
-        if (!(topVal in hash)) {
-            hash[topVal] = new Array(tops.length).fill('')
+            if(tops[i] !== target) topCount++
+            if(bottoms[i] !== target) botCount++
+
+            if(i === tops.length-1) ans = Math.min(topCount, botCount)
         }
-        if (!(botVal in hash)) {
-            hash[botVal] = new Array(tops.length).fill('')
-        }
-        set.add(topVal)
-        set.add(botVal)
-        hash[topVal][i] += 't'
-        hash[botVal][i] += 'b'
     }
-    let res = -1;
-    let arrRes = []
-    set.forEach((num) => {
-        const arr = hash[num].find((val) => val === "")
-        if (arr !== ''){
-            arrRes = hash[num]
-        }
-    })
-    let countT = 0
-    let countB = 0
-    for (const char of arrRes) {
-        if (char === 't') countT++
-        if (char === 'b') countB++
-    }
-    if (arrRes.length == 0) return res
-    return Math.min(countB, countT)
+
+    return ans
 };
