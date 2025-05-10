1f2129a76fe0c6ccf6decdbd761e8ebf13621bb2
Roger Perez <rogerprz@github.com>
2025-05-10 11:47:22 -0700

    Time: 81 ms (77.27%), Space: 74.1 MB (59.09%) - LeetHub

--- 2918-minimum-equal-sum-of-two-arrays-after-replacing-zeros/2918-minimum-equal-sum-of-two-arrays-after-replacing-zeros.js
+++ 2918-minimum-equal-sum-of-two-arrays-after-replacing-zeros/2918-minimum-equal-sum-of-two-arrays-after-replacing-zeros.js
@@ -3,38 +3,32 @@
  * @param {number[]} nums2
  * @return {number}
  */
-
 var minSum = function(nums1, nums2) {
-    let sum1 = 0;
-    let sum2 = 0;
-    let hasZero1 = false;
-    let hasZero2 = false;
-
-
-    for (let i = 0; i < nums1.length; i++) {
-        if (nums1[i] === 0) {
-            sum1++;
-            hasZero1 = true;
-        } else {
-            sum1 += nums1[i];
+    let zeroCount = 0 // 2
+    let zeroCount2 = 0; // 1
+    let numSum = 0;     // 6
+    let numSum2 = 0     // 11
+    // n1 = [3,2,0,1,0], 
+    // n2 = [6,5,0]
+    for (const num of nums1) {
+        if (num === 0) {
+            zeroCount++
+            numSum++
         }
+        else numSum += num
     }
-
-    for (let i = 0; i < nums2.length; i++) {
-        if (nums2[i] === 0) {
-            sum2++;
-            hasZero2 = true;
-        } else {
-            sum2 += nums2[i];
+    for (const num of nums2) {
+        if (num === 0) {
+            zeroCount2++
+            numSum2++
         }
+        else numSum2 += num
     }
-    if (sum1 > sum2 && !hasZero2) {
-        return -1;
+    if (numSum > numSum2 && zeroCount2 === 0) {
+        return -1
     }
-
-    if (sum2 > sum1 && !hasZero1) {
-        return -1;
+    if (numSum2 > numSum && zeroCount === 0) {
+        return -1
     }
-
-    return Math.max(sum1, sum2);
+    return Math.max(numSum, numSum2)
 };
