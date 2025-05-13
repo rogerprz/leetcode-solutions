34f0292e79ce0eac96ddb231d865e9960c1cb802
Roger Perez <rogerprz@github.com>
2025-05-12 20:59:53 -0700

    Time: 232 ms (100%), Space: 64.6 MB (37.5%) - LeetHub

--- 3335-total-characters-in-string-after-transformations-i/3335-total-characters-in-string-after-transformations-i.js
+++ 3335-total-characters-in-string-after-transformations-i/3335-total-characters-in-string-after-transformations-i.js
@@ -4,25 +4,23 @@
  * @return {number}
  */
 var lengthAfterTransformations = function(s, t) {
-    const MOD = 1e9 + 7;
-        const cnt = new Array(26).fill(0);
-
-        for (const c of s) {
-            cnt[c.charCodeAt(0) - 'a'.charCodeAt(0)]++;
+    const MOD = 1000000007;
+    let cnt = new Array(26).fill(0);
+    for (const ch of s) {
+        cnt[ch.charCodeAt(0) - "a".charCodeAt(0)]++;
+    }
+    for (let round = 0; round < t; round++) {
+        let nxt = new Array(26).fill(0);
+        nxt[0] = cnt[25];
+        nxt[1] = (cnt[25] + cnt[0]) % MOD;
+        for (let i = 2; i < 26; i++) {
+            nxt[i] = cnt[i - 1];
         }
-
-        for (let j = 0; j < t; j++) {
-            const tmp = new Array(26).fill(0);
-            for (let i = 0; i < 26; i++) {
-                if (i === 25) {
-                    tmp[0] = (tmp[0] + cnt[i]) % MOD;
-                    tmp[1] = (tmp[1] + cnt[i]) % MOD;
-                } else {
-                    tmp[i + 1] = (tmp[i + 1] + cnt[i]) % MOD;
-                }
-            }
-            cnt.splice(0, 26, ...tmp); // Update cnt with tmp values
-        }
-
-        return cnt.reduce((len, c) => (len + c) % MOD, 0);
+        cnt = nxt;
+    }
+    let ans = 0;
+    for (let i = 0; i < 26; i++) {
+        ans = (ans + cnt[i]) % MOD;
+    }
+    return ans;
 };
