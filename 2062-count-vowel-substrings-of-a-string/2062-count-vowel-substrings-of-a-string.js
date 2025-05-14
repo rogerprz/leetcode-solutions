cd0a4831e865d4b9d26d2a3d7a6dca823f15bbbf
Roger Perez <rogerprz@github.com>
2025-05-14 10:47:49 -0700

    Time: 10 ms (43.83%), Space: 56.9 MB (58.02%) - LeetHub

--- 2062-count-vowel-substrings-of-a-string/2062-count-vowel-substrings-of-a-string.js
+++ 2062-count-vowel-substrings-of-a-string/2062-count-vowel-substrings-of-a-string.js
@@ -5,19 +5,19 @@
 var countVowelSubstrings = function(word) {
     const vowels = new Set(['a','e','i','o','u'])
     const map = new Map()
+
     let total = 0;
-    const N = word.length;
 
-    for (let i = 0; i < N;i++) {
+    for (let i = 0; i < word.length;i++) {
         map.clear()
-        for (let j = i; j < N; j++) {
-            const subChar = word[j];
+        for (let j = i; j < word.length;j++) {
+            const subChar = word[j]
 
-            if (!vowels.has(subChar)) break
-            
+            if (!vowels.has(subChar)) break;
             map.set(subChar, true)
             if (map.size === 5) total++
         }
     }
-    return total;
+
+    return total
 };
