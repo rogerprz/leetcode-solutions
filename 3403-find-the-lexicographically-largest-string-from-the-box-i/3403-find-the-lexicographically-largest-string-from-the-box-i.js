a686849ff7e5e34826c5b178392e20658bf4102a
Roger Perez <rogerprz@github.com>
2025-06-03 22:01:41 -0700

    Time: 2 ms (86.67%), Space: 55.1 MB (100%) - LeetHub

--- 3403-find-the-lexicographically-largest-string-from-the-box-i/3403-find-the-lexicographically-largest-string-from-the-box-i.js
+++ 3403-find-the-lexicographically-largest-string-from-the-box-i/3403-find-the-lexicographically-largest-string-from-the-box-i.js
@@ -3,17 +3,33 @@
  * @param {number} numFriends
  * @return {string}
  */
+
+
 var answerString = function (word, numFriends) {
     if (numFriends === 1) {
         return word;
     }
-    let n = word.length;
-    let res = "";
-    for (let i = 0; i < n; i++) {
-        let s = word.substring(i, Math.min(i + n - numFriends + 1, n));
-        if (s > res) {
-            res = s;
+    let last = lastSubstring(word);
+    let n = word.length,
+        m = last.length;
+    return last.substring(0, Math.min(m, n - numFriends + 1));
+};
+var lastSubstring = function (s) {
+    let i = 0,
+        j = 1,
+        n = s.length;
+    while (j < n) {
+        let k = 0;
+        while (j + k < n && s[i + k] === s[j + k]) {
+            k++;
+        }
+        if (j + k < n && s[i + k] < s[j + k]) {
+            let t = i;
+            i = j;
+            j = Math.max(j + 1, t + k + 1);
+        } else {
+            j = j + k + 1;
         }
     }
-    return res;
+    return s.substring(i, n);
 };
