a9fb22f6e5d75dfac848bf837270676ce178b794
Roger Perez <rogerprz@github.com>
2025-06-03 22:32:42 -0700

    Time: 95 ms (96.77%), Space: 103.7 MB (41.94%) - LeetHub

--- 2034-stock-price-fluctuation/2034-stock-price-fluctuation.js
+++ 2034-stock-price-fluctuation/2034-stock-price-fluctuation.js
@@ -1,48 +1,48 @@
-const StockPrice = function () {
-	this.minHeap = new MinPriorityQueue({
-		compare: (a, b) => a.price > b.price,
-	});
-	this.maxHeap = new MaxPriorityQueue({
-		compare: (a, b) => a.price < b.price,
-	});
-	this.prices = new Map();
-	this.currentPrice = {};
-};
+class StockPrice {
+    constructor() {
+        this.timeMap = new Map()
+        this.priceCount = new Map()
+        this.maxPrice = -Infinity
+        this.minPrice = Infinity
+        this.lastTime = 0
+    }
 
-StockPrice.prototype.update = function (timestamp, price) {
-	this.prices.set(timestamp, price);
-	this.minHeap.enqueue({ timestamp, price });
-	this.maxHeap.enqueue({ timestamp, price });
-	if (
-		!this.currentPrice.timestamp ||
-		this.currentPrice.timestamp <= timestamp
-	) {
-		this.currentPrice = { timestamp, price };
-	}
-};
+    update(timestamp, price) {
+        if (this.timeMap.has(timestamp)) {
+            const oldPrice = this.timeMap.get(timestamp)
 
-StockPrice.prototype.current = function () {
-	return this.currentPrice.price;
-};
+            const oldCount = this.priceCount.get(oldPrice)
+            if (oldCount === 1) {
+                this.priceCount.delete(oldPrice)
+            } else {
+                this.priceCount.set(oldPrice, oldCount -1)
+            }
+        }
 
-StockPrice.prototype.maximum = function () {
-	let front = this.maxHeap.front();
+        this.timeMap.set(timestamp, price)
+        this.lastTime = Math.max(this.lastTime, timestamp)
 
-	while (this.prices.get(front.timestamp) != front.price) {
-		this.maxHeap.dequeue();
-		front = this.maxHeap.front();
-	}
+        this.priceCount.set(price, (this.priceCount.get(price) || 0) +1)
+        if (price > this.maxPrice ) this.maxPrice = price
+        if (price < this.minPrice) this.minPrice = price
 
-	return front.price;
-};
+    }
 
-StockPrice.prototype.minimum = function () {
-	let front = this.minHeap.front();
+    current() {
+        return this.timeMap.get(this.lastTime)
+    }
 
-	while (this.prices.get(front.timestamp) != front.price) {
-		this.minHeap.dequeue();
-		front = this.minHeap.front();
-	}
+    maximum() {
+        while (!this.priceCount.has(this.maxPrice)) {
+            this.maxPrice -=1
+        }
+        return this.maxPrice
+    }
 
-	return front.price;
-};
+    minimum() {
+        while (!this.priceCount.has(this.minPrice)) {
+            this.minPrice +=1
+        }
+        return this.minPrice
+    }
+}
