7285c1f2d3a3d342fe5ecc2a063ff3572d90bb1a
Roger Perez <rogerprz@github.com>
2025-07-27 13:20:59 -0700

    Time: 355 ms (78.88%), Space: 153.6 MB (52.8%) - LeetHub

--- 3607-power-grid-maintenance/3607-power-grid-maintenance.js
+++ 3607-power-grid-maintenance/3607-power-grid-maintenance.js
@@ -1,157 +1,47 @@
-/**
- * @param {number} numberOfPowerStations
- * @param {number[][]} connections
- * @param {number[][]} queries
- * @return {number[]}
- */
-// Power Stations have 1‑based indexing.
-var processQueries = function (numberOfPowerStations, connections, queries) {
-    const unionFind = new UnionFind(numberOfPowerStations);
-    for (let connection of connections) {
-        unionFind.joinByRank(connection[0], connection[1]);
-    }
-
-    // Map<Integer, OperationalStations> 
-    const parentToOperationalStations = createParentToOperationalStations(unionFind, numberOfPowerStations);
-
-    return assignStationsDuringMaintenance(queries, unionFind, parentToOperationalStations);
-};
-
-/**
- * @param {UnionFind} unionFind
- * @param {number} numberOfPowerStations
- * @return {Map<Integer, OperationalStations> []}
- */
-function createParentToOperationalStations(unionFind, numberOfPowerStations) {
-    const parentToOperationalStations = new Map();
-
-    for (let station = 1; station <= numberOfPowerStations; ++station) {
-        const parent = unionFind.findParent(station);
-        if (!parentToOperationalStations.has(parent)) {
-            parentToOperationalStations.set(parent, new OperationalStations());
-        }
-        parentToOperationalStations.get(parent).addStation(station);
-    }
-    return parentToOperationalStations;
-}
-
-/**
- * @param {number[][]} queries
- * @param {UnionFind} unionFind
- * @param {Map<number, OperationalStations>} parentToOperationalStations
- * @return {number[]} 
- */
-function assignStationsDuringMaintenance(queries, unionFind, parentToOperationalStations) {
-    const resultsOfQueryForMaintenanceCheck = new Array();
-
-    for (let query of queries) {
-        const type = query[0];
-        const station = query[1];
-        const parent = unionFind.findParent(station);
-        if (type === Util.GO_OFFLINE_REQUEST) {
-            parentToOperationalStations.get(parent).removeStation(station);
-            continue;
-        }
-
-        if (type === Util.MAINTENANCE_CHECK_REQUEST) {
-            const result = parentToOperationalStations.get(parent).getStationToTakeOverDuringMaintenanceCheck(station);
-            resultsOfQueryForMaintenanceCheck.push(result);
-        }
-    }
-
-    return resultsOfQueryForMaintenanceCheck;
-}
-
 class UnionFind {
-
-    /**
-     * @param {number} numberOfPowerStations
-     */
-    constructor(numberOfPowerStations) {
-        this.parent = Array.from(Array(numberOfPowerStations + 1).keys());
-        this.rank = new Array(numberOfPowerStations + 1).fill(1);
+    constructor(size) {
+        this.size = size;
+        this.uf = Array.from(Array(size), (_, idx) => idx);
     }
-
-    /**
-     * @param {number} index
-     * @return {number}
-     */
-    findParent(index) {
-        if (this.parent[index] !== index) {
-            this.parent[index] = this.findParent(this.parent[index]);
-        }
-        return this.parent[index];
+    find(a) {
+        if (a !== this.uf[a]) this.uf[a] = this.find(this.uf[a]);
+        return this.uf[a];
     }
+    union(a, b) {
+        this.uf[this.find(a)] = this.find(b);
+    }
+}
 
-    /**
-     * @param {number} indexOne
-     * @param {number} indexTwo
-     * @return {void}
-     */
-    joinByRank(indexOne, indexTwo) {
-        const first = this.findParent(indexOne);
-        const second = this.findParent(indexTwo);
-        if (first === second) {
-            return;
-        }
-
-        if (this.rank[first] >= this.rank[second]) {
-            this.parent[second] = first;
-            this.rank[first] += this.rank[second];
+const processQueries = function(c, connections, queries) {
+    const result = [];
+    const statuses = Array(c).fill(true);
+    const grids = Array.from(Array(c), () => []);
+    const uf = new UnionFind(c);
+    for (const [a, b] of connections) {
+        uf.union(a - 1, b - 1);
+    }
+    for (let i = 0; i < c; ++i) {
+        const root = uf.find(i);
+        grids[root].push(i);
+    }
+    for (const grid of grids) {
+        grid.sort((a, b) => b - a);
+    }
+    for (const [type, station] of queries) {
+        if (type === 1) {
+            if (statuses[station-1]) {
+                result.push(station);
+            } else {
+                const grid = grids[uf.find(station-1)];
+                while (grid.length && !statuses[grid.at(-1)]) grid.pop();
+                if (grid.length) result.push(grid.at(-1) + 1);
+                else result.push(-1);
+            }
+        } else if (type === 2) {
+            statuses[station-1] = false;
         } else {
-            this.parent[first] = second;
-            this.rank[second] += this.rank[first];
+            throw `Invalid type: ${type}`;
         }
     }
-}
-
-class OperationalStations {
-
-    // Set<number> 
-    operationalStations = new Set();
-    // PriorityQueue<number>
-    minHeapForStationID = new PriorityQueue((x, y) => x - y);
-
-    /**
-     * @param {number} station
-     * @return {void}
-     */
-    addStation(station) {
-        this.operationalStations.add(station);
-        this.minHeapForStationID.enqueue(station);
-    }
-
-    /**
-     * @param {number} station
-     * @return {void}
-     */
-    removeStation(station) {
-        if (this.operationalStations.has(station)) {
-            this.operationalStations.delete(station);
-        }
-    }
-
-    /**
-     * @param {number} station
-     * @return {number}
-     */
-    getStationToTakeOverDuringMaintenanceCheck(station) {
-        if (this.operationalStations.size === 0) {
-            return Util.NO_OPERATIONAL_STATION_EXISTS_IN_THIS_GRID;
-        }
-        if (this.operationalStations.has(station)) {
-            return station;
-        }
-
-        while (!this.minHeapForStationID.isEmpty() && !this.operationalStations.has(this.minHeapForStationID.front())) {
-            this.minHeapForStationID.dequeue();
-        }
-        return this.minHeapForStationID.front();
-    }
-}
-
-class Util {
-    static NO_OPERATIONAL_STATION_EXISTS_IN_THIS_GRID = -1;
-    static MAINTENANCE_CHECK_REQUEST = 1;
-    static GO_OFFLINE_REQUEST = 2;
-}
+    return result;
+};
