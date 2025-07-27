6892b1c6d265b18c88125e4a35770292dd0d94eb
Roger Perez <rogerprz@github.com>
2025-07-26 22:00:45 -0700

    Time: 0 ms (100%), Space: 55.2 MB (26.15%) - LeetHub

---
+++ 2210-count-hills-and-valleys-in-an-array/2210-count-hills-and-valleys-in-an-array.js
@@ -0,0 +1,40 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var countHillValley = function(nums) {
+    let res = 0; 
+    const N = nums.length;
+
+    for (let i = 1; i < N - 1; i++) {
+        if (nums[i] === nums[i-1]) {
+            // de-dupe
+            continue
+        }
+        let left = 0; 
+        for (let j = i - 1; j >=0; --j) {
+            if (nums[j] > nums[i]) {
+                left = 1;
+                break;
+            } else if (nums[j] < nums[i]) {
+                left = -1;
+                break;
+            }
+        }
+        let right = 0;
+        for (let j = i + 1; j < N; ++j) {
+            if (nums[j] > nums[i]) {
+                right = 1;
+                break;
+            } else if (nums[j] < nums[i]) {
+                right = -1;
+                break;
+            }
+        }
+        if (left === right && left !== 0) {
+            // at this time, index i is part of a peak or valley.
+            ++res;
+        }
+    }
+    return res
+};
