0058be19e240da4779f50628eaa13758056ea822
Roger Perez <rogerprz@github.com>
2025-07-27 15:35:29 -0700

    Time: 0 ms (100%), Space: 54.3 MB (41.54%) - LeetHub

--- 2210-count-hills-and-valleys-in-an-array/2210-count-hills-and-valleys-in-an-array.js
+++ 2210-count-hills-and-valleys-in-an-array/2210-count-hills-and-valleys-in-an-array.js
@@ -8,9 +8,6 @@ var countHillValley = function(nums) {
     for (let i = 1; i < nums.length;i++) {
         const num = nums[i]
         if (num === nums[i+1]) continue
-        // [2,4,1,1,6,5]
-        //        l i r
-        // 
         const isHill = num > nums[left] && num > nums[i+1]
         const isValley = num < nums[left] && num < nums[i+1]
 
