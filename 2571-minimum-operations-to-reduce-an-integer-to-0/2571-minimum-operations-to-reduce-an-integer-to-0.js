3c7dd45792de1fd6f764cdea895297d15678c756
Roger Perez <rogerprz@github.com>
2025-07-27 11:30:59 -0700

    Time: 0 ms (100%), Space: 53.6 MB (46.77%) - LeetHub

--- 2571-minimum-operations-to-reduce-an-integer-to-0/2571-minimum-operations-to-reduce-an-integer-to-0.js
+++ 2571-minimum-operations-to-reduce-an-integer-to-0/2571-minimum-operations-to-reduce-an-integer-to-0.js
@@ -3,15 +3,17 @@
  * @return {number}
  */
 var minOperations = function(n) {
-     let oneArr = [];
-  let ans = 0;
-  while (n !== 0) {
-    if (n % 2 === 1) {
-      ans++;
-      if (n === 1 || (n & 2) === 0) n--;
-      else n++;
-    } else n /= 2;
-  }
+    let oneArr = [];
+    let ans = 0;
+    while (n !== 0) {
+        if (n % 2 === 1) {
+            ans++;
+            if (n === 1 || (n & 2) === 0) n--;
+            else n++;
+        } else {
+            n = n/ 2;
+        }
+    }
 
-  return ans;
+    return ans;
 };
