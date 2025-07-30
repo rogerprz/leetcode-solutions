b91c507be8686706ef0c616fe709732e1a3e2cb9
Roger Perez <rogerprz@github.com>
2025-07-29 22:57:05 -0700

    Time: 1 ms (84.67%), Space: 57.5 MB (56.12%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.js
@@ -0,0 +1,55 @@
+/**
+ * @param {character[]} chars
+ * @return {number}
+ */
+var compress = function(chars) {
+    
+    const stack = []; // [{s: b, c: 1}]
+    const res = []    // [a, 2, ]
+    for (let i = 0; i < chars.length;i++) {
+        const char = chars[i]
+    // ["a","a","b","b","c","c","c"]
+    //       i    
+        if (stack.length === 0) {
+            stack.push({char, count: 1})
+        } else {
+            const top = stack[stack.length -1]
+
+            if (top.char === char) {
+                top.count++
+            } else {
+                res.push(top.char)
+                if (top.count !== 1) {
+                    if (top.count >= 10) {
+                        const nums = top.count.toString()
+                        nums.split("").forEach((num)=> {
+                            res.push(num)
+                        })
+                    } else {
+                        res.push(top.count.toString())
+                    }
+                }
+                stack.pop()
+                stack.push({char, count:1})
+            }
+        }
+     }
+     if (stack.length > 0) {
+        const top = stack.pop()
+         res.push(top.char)
+         if (top.count !== 1) {
+            if (top.count >= 10) {
+                const nums = top.count.toString()
+                nums.split("").forEach((num)=> {
+                    res.push(num)
+                })
+            } else {
+                res.push(top.count.toString())
+            }
+        }
+     }
+     for (let i = 0; i < res.length;i++) {
+        chars[i] = res[i]
+     }
+     return res.length
+};
