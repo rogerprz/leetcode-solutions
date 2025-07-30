a7bdc46380c59d467d55c9725c5d87fa55872d17
Roger Perez <rogerprz@github.com>
2025-07-29 23:06:35 -0700

    Time: 0 ms (100%), Space: 58 MB (31.71%) - LeetHub

--- 0443-string-compression/0443-string-compression.js
+++ 0443-string-compression/0443-string-compression.js
@@ -3,53 +3,28 @@
  * @return {number}
  */
 var compress = function(chars) {
-    
-    const stack = []; // [{s: b, c: 1}]
-    let res = ''    // [a, 2, ]
-    for (let i = 0; i < chars.length;i++) {
-        const char = chars[i]
-    // ["a","a","b","b","c","c","c"]
-    //       i    
-        if (stack.length === 0) {
-            stack.push({char, count: 1})
-        } else {
-            const top = stack[stack.length -1]
+    let l = 0;
+    let r = 0;
 
-            if (top.char === char) {
-                top.count++
-            } else {
-                res += (top.char)
-                if (top.count !== 1) {
-                    if (top.count >= 10) {
-                        const nums = top.count.toString()
-                        nums.split("").forEach((num)=> {
-                            res += (num)
-                        })
-                    } else {
-                        res += (top.count.toString())
-                    }
-                }
-                stack.pop()
-                stack.push({char, count:1})
+    while(l < chars.length){
+        const char = chars[l];
+        let count = 0;
+
+        while(l < chars.length && chars[l] === char){
+            l++;
+            count++;
+        }
+
+        chars[r++] = char;
+
+        if(count > 1){
+            const countStr = count.toString();
+
+            for(let digit of countStr){
+                chars[r++] = digit;
             }
         }
-     }
-     if (stack.length > 0) {
-        const top = stack.pop()
-         res += (top.char)
-         if (top.count !== 1) {
-            if (top.count >= 10) {
-                const nums = top.count.toString()
-                nums.split("").forEach((num)=> {
-                    res += (num)
-                })
-            } else {
-                res += (top.count.toString())
-            }
-        }
-     }
-     for (let i = 0; i < res.length;i++) {
-        chars[i] = res[i]
-     }
-     return res.length
+    }
+
+    return r;
 };
