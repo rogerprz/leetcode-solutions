c625834022632d4b9f4990dfb6d8049f4a6b289b
Roger Perez <rogerprz@github.com>
2025-08-03 12:58:53 -0700

    Time: 2 ms (98.55%), Space: 59.2 MB (62.41%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.js
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.js
@@ -2,11 +2,45 @@
  * @param {number[]} nums
  * @return {number}
  */
-var maximumProduct = function(nums) {
-    nums.sort((a,b) => a-b)
-    const len = nums.length -1
-    const min = nums[0] * nums[1] * nums[len]
-    const max = nums[len] * nums[len-1] * nums[len-2]
 
-    return Math.max(min, max)
+var maximumProduct = function(nums) {
+    let minA = Infinity;
+    let minB = Infinity;
+    let minC = Infinity;
+
+    let maxA = -Infinity;
+    let maxB = -Infinity;
+    let maxC = -Infinity;
+
+    for (let i = 0; i < nums.length; i++) {
+        const cur = nums[i];
+        if (cur <= minA) {
+            minC = minB;
+            minB = minA;
+            minA = cur;
+        } else if (cur <= minB ) {
+            minC = minB;
+            minB = cur;
+
+        } else if (cur <= minC) {
+            minC = cur;
+        }
+
+        if (cur >= maxA) {
+            maxC = maxB;
+            maxB = maxA;
+            maxA = cur;
+        } else if (cur >= maxB) {
+            maxC = maxB;
+            maxB = cur;
+        } else if (cur >= maxC) {
+            maxC = cur;
+        }
+
+    }
+
+    const minPrd = minA * minB * maxA;
+    const maxPrd = maxA * maxB * maxC;
+
+    return Math.max(minPrd, maxPrd);
 };
