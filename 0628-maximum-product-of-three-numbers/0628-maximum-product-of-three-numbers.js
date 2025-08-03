6127ed998dc3d47c886de876c38539e352b09151
Roger Perez <rogerprz@github.com>
2025-08-03 12:57:24 -0700

    Time: 40 ms (15.18%), Space: 58.9 MB (75.18%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.js
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.js
@@ -3,9 +3,10 @@
  * @return {number}
  */
 var maximumProduct = function(nums) {
-    if (nums.length < 3) return 0;
     nums.sort((a,b) => a-b)
-    const negMax = nums[0] * nums[1] * nums[nums.length -1]
-    const end = nums[nums.length - 1] * nums[nums.length -2] * nums[nums.length -3]
-    return Math.max(negMax, end)
+    const len = nums.length -1
+    const min = nums[0] * nums[1] * nums[len]
+    const max = nums[len] * nums[len-1] * nums[len-2]
+
+    return Math.max(min, max)
 };
