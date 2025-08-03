55ba8d67e1e3a69a494f8fb120915e096a7faf6f
Roger Perez <rogerprz@github.com>
2025-08-03 15:24:33 -0700

    Time: 97 ms (94.74%), Space: 85.7 MB (100%) - LeetHub

--- 3488-closest-equal-element-queries/3488-closest-equal-element-queries.js
+++ 3488-closest-equal-element-queries/3488-closest-equal-element-queries.js
@@ -6,16 +6,16 @@
 var solveQueries = function(nums, queries) {
     const last = {}
     const close = new Array(nums.length * 2).fill(nums.length)
-    for (let i = 0; i < 2 * nums.length; ++i) {
+    for (let i = 0; i < close.length; ++i) {
         const pos = i % nums.length
-        const n = nums[pos]
-        if (last[n] !== undefined) {
-            close[i] = i - last[n]
-            if (close[last[n]] > close[i]) {
-                close[last[n]] = close[i]
+        const num = nums[pos]
+        if (num in last) {
+            close[i] = i - last[num]
+            if (close[last[num]] > close[i]) {
+                close[last[num]] = close[i]
             }
         }
-        last[n] = i
+        last[num] = i
     }
     return queries.map(q => {
         const d = Math.min(close[q], close[q + nums.length])
