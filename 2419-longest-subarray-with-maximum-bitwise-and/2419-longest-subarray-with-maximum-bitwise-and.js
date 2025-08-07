00e2cc8519d8572f9810d709cfec9874edce8d8c
Roger Perez <rogerprz@github.com>
2025-08-06 22:21:18 -0700

    Time: 5 ms (41.76%), Space: 69.3 MB (5.33%) - LeetHub

--- 2419-longest-subarray-with-maximum-bitwise-and/2419-longest-subarray-with-maximum-bitwise-and.js
+++ 2419-longest-subarray-with-maximum-bitwise-and/2419-longest-subarray-with-maximum-bitwise-and.js
@@ -3,20 +3,19 @@
  * @return {number}
  */
 var longestSubarray = function(nums) {
-    let max = Math.max(...nums);
-    let maxLen = 0;
-    let streak = 0;
-    // [1,2,3,3,2,2]
-    for (let num of nums) {
-        if (num === max) {
-            streak++;
-            maxLen = Math.max(maxLen, streak);
-        } else {
-            streak = 0;
+    const maxNum = Math.max(...nums);
+
+    let count = 0;
+    let ans = 0
+    for (const num of nums) {
+        if (num === maxNum) count++
+        if (num < maxNum) {
+            ans = Math.max(count,ans)
+            count = 0;
+            continue
         }
+        
     }
 
-    
-    return maxLen;
-
+    return Math.max(count, ans);
 };
