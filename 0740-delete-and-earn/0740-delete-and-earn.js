8c38cb5fd0409c7dcfeb264c8e96edeb1100a7a7
Roger Perez <rogerprz@github.com>
2025-08-06 20:37:02 -0700

    Time: 17 ms (17.83%), Space: 60.2 MB (27.91%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.js
+++ 0740-delete-and-earn/0740-delete-and-earn.js
@@ -3,35 +3,38 @@
  * @return {number}
  */
 var deleteAndEarn = function(nums) {
-    const set = new Set()
-    const freq = {}
+    const set = new Set();
+    const map = new Map()
 
-    for (const num of nums) {
-        if (!(set.has(num))) {
-            freq[num] = 0
+    for (let i = 0; i < nums.length;i++) {
+        const num = nums[i]
+        if (!set.has(num)) {
+            map.set(num, 0)
             set.add(num)
         }
-        freq[num] += num
+        const prevNum = map.get(num) + num
+        map.set(num, prevNum)
     }
 
     const uniq = [...set].sort((a,b) => a - b);
-    let take = 0;
-    let skip = 0
-    let prev = null;
-    for (const num of uniq) {
-        const currPoints = freq[num];
+    let take = 0;    // 3
+    let skip = 0;    // 2
+    let prev = null; // 3
+    // [2,3,4]
 
-        const maxPrev = Math.max(skip, take)
+    for (const num of uniq) {
+        const currPoints = map.get(num) // 4
+        const maxPrev = Math.max(take, skip) // 3
 
         if (prev === num - 1) {
-            take = currPoints + skip 
+            take = currPoints + skip;
             skip = maxPrev;
         } else {
             take = currPoints + maxPrev;
-            skip = maxPrev
+            skip = maxPrev;
         }
-        prev = num
+        prev = num;
     }
 
-    return Math.max(skip, take)
+    return Math.max(take, skip)
 };
