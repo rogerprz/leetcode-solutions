a700d2524d72b9e44bbfdacdfb58bafd8b6562aa
Roger Perez <rogerprz@github.com>
2025-08-06 22:08:45 -0700

    Time: 105 ms (76.19%), Space: 89.3 MB (71.43%) - LeetHub

--- 3488-closest-equal-element-queries/3488-closest-equal-element-queries.js
+++ 3488-closest-equal-element-queries/3488-closest-equal-element-queries.js
@@ -5,26 +5,26 @@
  */
 var solveQueries = function(nums, queries) {
     const last = {}
+    const n = nums.length;
+    let close = new Array(n * 2).fill(n);
 
-    const close = new Array(nums.length * 2).fill(nums.length)
+    for (let i = 0; i < close.length;i++) {
+        const pos = i % n;
+        const num = nums[pos];
 
-    for (let i = 0; i < close.length; ++i) {
-        // i = 4
-        const pos = i % nums.length // 0
-        const num = nums[pos] // 1
-        // last[num] = 3
         if (num in last) {
-            // curr distance
-            close[i] = i - last[num]  // 3 - 0 = 0
-            const prevDist = close[last[num]]
-            if (prevDist > close[i]) {
-                close[last[num]] = close[i]
+            close[i] = i - last[num]; // currPos
+            const prevPos = close[last[num]]
+
+            if (prevPos > close[i]) {
+                close[last[num]] = close[i];
             }
         }
-        last[num] = i
+        last[num] = i;
     }
-    return queries.map(q => {
-        const d = Math.min(close[q], close[q + nums.length])
-        return d === nums.length ? -1 : d
+
+    return queries.map((q) => {
+        const d = Math.min(close[q], close[q + n]);
+        return d === n ? -1 : d;
     })
 };
