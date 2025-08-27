d0fe24886a8547b5c3b01caa7117b0841a23f8dd
Roger Perez <rogerprz@github.com>
2025-08-27 00:39:58 -0700

    Time: 12 ms (79.55%), Space: 55.4 MB (47.73%) - LeetHub

--- 0465-optimal-account-balancing/0465-optimal-account-balancing.js
+++ 0465-optimal-account-balancing/0465-optimal-account-balancing.js
@@ -2,41 +2,39 @@
  * @param {number[][]} transactions
  * @return {number}
  */
-function minTransfers(transactions) {
-    const balance = {};
+var minTransfers = function(transactions) {
+    const balance = {}
 
-    // Calculate net balance of each person
     for (const [from, to, amount] of transactions) {
-        balance[from] = (balance[from] || 0) - amount;
-        balance[to] = (balance[to] || 0) + amount;
+        if (!(from in balance)) balance[from]= 0
+        if (!(to in balance)) balance[to]= 0
+
+        balance[from] -= amount
+        balance[to] += amount
     }
 
-    // Filter out persons with non-zero balances
-    const debts = Object.values(balance).filter(amount => amount !== 0);
+    const debts = Object.values(balance).filter(amount => amount != 0)
 
-    // Helper function for DFS
     function dfs(start) {
         while (start < debts.length && debts[start] === 0) {
-            start++;
+            start++
         }
-
         if (start === debts.length) return 0;
 
         let minTransactions = Infinity;
-        for (let i = start + 1; i < debts.length; i++) {
-            if (debts[start] * debts[i] < 0) {  // must have opposite signs
-                // Settle debts[start] with debts[i]
-                debts[i] += debts[start];
 
-                minTransactions = Math.min(minTransactions, 1 + dfs(start + 1));
+        for (let i = start + 1; i < debts.length;i++) {
+            if (debts[start] * debts[i] < 0) {
 
-                // Backtrack
-                debts[i] -= debts[start];
+                debts[i] += debts[start]
+
+                minTransactions = Math.min(minTransactions, 1 + dfs(start + 1))
+
+                debts[i] -= debts[start]
             }
         }
-
-        return minTransactions;
+        return minTransactions
     }
 
-    return dfs(0);
-}
+    return dfs(0)
+};
