12d414eace5e1cef99574e8b43df1db62a4e2cee
Roger Perez <rogerprz@github.com>
2025-08-26 20:26:12 -0700

    Time: 1 ms (85.82%), Space: 54.2 MB (99.25%) - LeetHub

--- 0443-string-compression/0443-string-compression.js
+++ 0443-string-compression/0443-string-compression.js
@@ -3,27 +3,28 @@
  * @return {number}
  */
 var compress = function(chars) {
-    let l = 0; 
+    
+    let l = 0;
     let r = 0;
-    // ["a","a","b","b","c","c","c"]
-    //.         l.   r 
-    //   cn
-    // count=2
     while (r < chars.length) {
-        let count = 0; 
-        const char = chars[r];
-        while (char === chars[r]) {
-            count++
+        const curr = chars[r];
+        let count = 0;
+        // c: 
+        // [a,a,b,b,c,c,c]
+        //  l r
+        while (curr === chars[r]) {
+            count++;
             r++
         }
-        chars[l++] = char
-        if (count <= 1) continue 
-        const countStr = count.toString()
-        for (const num of countStr) {
-            chars[l] = num;
+        chars[l++] = curr;
+        if (count <= 1) continue;
+
+        for (const digit of count.toString()) {
+            chars[l] = digit;
             l++
         }
+
     }
 
-    return l
+    return l;
 };
