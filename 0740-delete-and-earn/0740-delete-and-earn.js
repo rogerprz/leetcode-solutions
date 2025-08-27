348376055b13ded84a068dafc6708b6df03b02bf
Roger Perez <rogerprz@github.com>
2025-08-27 11:38:52 -0700

    Time: 12 ms (44.72%), Space: 58.1 MB (73.91%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.js
+++ 0740-delete-and-earn/0740-delete-and-earn.js
@@ -3,35 +3,38 @@
  * @return {number}
  */
 var deleteAndEarn = function(nums) {
-    const set = new Set();
     const map = new Map();
+    const set = new Set();
 
     for (const num of nums) {
         if (!set.has(num)) {
+            map.set(num, 0)
             set.add(num)
-            map.set(num,  0)
         }
         map.set(num, map.get(num) + num)
+        set.add(num)
     }
 
-    const uniq = [...set].sort((a,b) => a - b)
+    const uniq = [...set].sort((a,b) => a-b)
 
     let take = 0;
     let skip = 0;
     let prev = null;
-    
-    for (const currNum of uniq) {
-        const currPoints = map.get(currNum);
+
+    for (let i = 0; i < uniq.length;i++) {
+        const curr = uniq[i]
+        const currPoints = map.get(curr);
         const maxPrev = Math.max(take, skip)
 
-        if (prev === currNum - 1) {
-            take = currPoints + skip
+        if (curr - 1 === prev) {
+            // curr == prev 
+            take = currPoints + skip;
             skip = maxPrev;
         } else {
             take = currPoints + maxPrev;
             skip = maxPrev
         }
-        prev = currNum
+        prev = curr
     }
     return Math.max(take, skip)
 };
