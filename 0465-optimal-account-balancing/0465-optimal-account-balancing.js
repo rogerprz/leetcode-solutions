3d54508c87c726d147d4d205489afaca2332174c
Roger Perez <rogerprz@github.com>
2025-08-27 08:18:16 -0700

    Time: 6 ms (95.45%), Space: 60.1 MB (6.82%) - LeetHub

--- 0465-optimal-account-balancing/0465-optimal-account-balancing.js
+++ 0465-optimal-account-balancing/0465-optimal-account-balancing.js
@@ -6,34 +6,34 @@ var minTransfers = function(transactions) {
     const balance = {}
 
     for (const [from, to, amount] of transactions) {
-        if (!(from in balance)) balance[from]= 0
-        if (!(to in balance)) balance[to]= 0
-
-        balance[from] -= amount
+        if (!(from in balance)) balance[from] = 0
+        if (!(to in balance)) balance[to] = 0
+        balance[from] -= amount 
         balance[to] += amount
     }
 
-    const debts = Object.values(balance).filter(amount => amount != 0)
+    const debts = Object.values(balance).filter((amount) => amount != 0)
 
-    function dfs(start) {
+    const dfs = (start) => {
         while (start < debts.length && debts[start] === 0) {
             start++
         }
+
         if (start === debts.length) return 0;
 
-        let minTransactions = Infinity;
-
-        for (let i = start + 1; i < debts.length;i++) {
-            if (debts[start] * debts[i] < 0) {
-
+        let minTransaction = Infinity;
+        const seen = new Set();
+        for (let i = start + 1; i < debts.length; i++) {
+            if (debts[start] * debts[i] < 0 && !seen.has(debts[i])) {
+                seen.add(debts[i]);
                 debts[i] += debts[start]
 
-                minTransactions = Math.min(minTransactions, 1 + dfs(start + 1))
+                minTransaction = Math.min(minTransaction, 1 + dfs(start + 1))
 
                 debts[i] -= debts[start]
             }
         }
-        return minTransactions
+        return minTransaction
     }
 
     return dfs(0)
