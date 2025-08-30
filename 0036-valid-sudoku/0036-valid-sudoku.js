354c16e58b9e4da10ee4ae7ab64fec397007c4fa
Roger Perez <rogerprz@github.com>
2025-08-30 12:22:38 -0700

    Time: 4 ms (77.35%), Space: 57.4 MB (52.98%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.js
+++ 0036-valid-sudoku/0036-valid-sudoku.js
@@ -3,10 +3,6 @@
  * @return {boolean}
  */
 var isValidSudoku = function(board) {
-    // we need to create a set to find if row has repeating value
-    // we need to create a set to save column values
-    // we need to get a set so we can detect box. 
-
     // loop through first array
     for (let i = 0; i< board.length;i++) {
         const row = new Set();
@@ -16,10 +12,10 @@ var isValidSudoku = function(board) {
         for (let j = 0; j < board[i].length; j++) {
             const currRow = board[i][j]
             const currCol = board[j][i]
-
-            const xIdx = 3 * Math.floor(i / 3) + Math.floor(j / 3)
+            
+            const xIdx = 3 * Math.floor(i/3) + Math.floor(j / 3)
             const yIdx = ((i * 3) % 9) + (j % 3)
-            const currBox = board[xIdx][yIdx]
+            const currBox =  board[xIdx][yIdx]
 
             if (row.has(currRow)) return false 
             if (col.has(currCol)) return false 
