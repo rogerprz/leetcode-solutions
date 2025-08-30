0716dd86042b2e48a8b924e91b5b002a59f4c661
Roger Perez <rogerprz@github.com>
2025-08-30 15:18:17 -0700

    Time: 216 ms (96.43%), Space: 98.1 MB (87.5%) - LeetHub

--- 1834-single-threaded-cpu/1834-single-threaded-cpu.js
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.js
@@ -1,93 +1,32 @@
-class MinHeap {
-  constructor() {
-    this.heap = [];
-  }
+var getOrder = function (tasks) {
+    if (!tasks || tasks.length === 0) return [];
+    // sort by start time and task duration
+    const sortedTasks = tasks
+        .map((e, i) => { return { startTime: e[0], duration: e[1], index: i } })
+        .sort((a, b) => a.startTime - b.startTime);
+    // {startTime: x, duration, y, index: z}
+    const minQueue = new PriorityQueue((a, b) => {
+        if (a.duration === b.duration) return a.index - b.index;
+        else return a.duration - b.duration;
+    })
 
-  // Compare by processingTime first, then index
-  compare(a, b) {
-    if (a[0] === b[0]) return a[1] - b[1];
-    return a[0] - b[0];
-  }
-
-  push(val) {
-    this.heap.push(val);
-    this.bubbleUp();
-  }
-
-  pop() {
-    if (this.size() === 0) return null;
-    const top = this.heap[0];
-    const end = this.heap.pop();
-    if (this.size() > 0) {
-      this.heap[0] = end;
-      this.bubbleDown();
-    }
-    return top;
-  }
-
-  bubbleUp() {
-    let i = this.heap.length - 1;
-    while (i > 0) {
-      let parent = Math.floor((i - 1) / 2);
-      if (this.compare(this.heap[i], this.heap[parent]) < 0) {
-        [this.heap[i], this.heap[parent]] = [this.heap[parent], this.heap[i]];
-        i = parent;
-      } else break;
-    }
-  }
-
-  bubbleDown() {
+    let cpuStartTime = 0;
+    const ans = [];
     let i = 0;
-    const n = this.heap.length;
-    while (true) {
-      let left = 2 * i + 1;
-      let right = 2 * i + 2;
-      let smallest = i;
+    while (i < sortedTasks.length || !minQueue.isEmpty()) {
+        // next task if queue empty
+        if (minQueue.isEmpty() && cpuStartTime < sortedTasks[i].startTime) {
+            cpuStartTime = sortedTasks[i].startTime;
+        }
+        while (i < sortedTasks.length && cpuStartTime >= sortedTasks[i].startTime) {
+            minQueue.enqueue(sortedTasks[i]);
+            i += 1;
+        }
 
-      if (left < n && this.compare(this.heap[left], this.heap[smallest]) < 0) {
-        smallest = left;
-      }
-      if (right < n && this.compare(this.heap[right], this.heap[smallest]) < 0) {
-        smallest = right;
-      }
-      if (smallest !== i) {
-        [this.heap[i], this.heap[smallest]] = [this.heap[smallest], this.heap[i]];
-        i = smallest;
-      } else break;
-    }
-  }
-
-  size() {
-    return this.heap.length;
-  }
-}
-
-var getOrder = function(tasks) {
-  // Store (enqueue, process, index)
-  const sortedTasks = tasks.map((t, i) => [t[0], t[1], i]).sort((a, b) => a[0] - b[0]);
-
-  const result = [];
-  const heap = new MinHeap();
-
-  let time = 0;
-  let taskIndex = 0;
-
-  while (taskIndex < sortedTasks.length || heap.size() > 0) {
-    if (heap.size() === 0 && time < sortedTasks[taskIndex][0]) {
-      time = sortedTasks[taskIndex][0];
+        const nextTask = minQueue.dequeue();
+        cpuStartTime += nextTask.duration;
+        ans.push(nextTask.index);
     }
 
-    // Push all tasks that have arrived by current time
-    while (taskIndex < sortedTasks.length && sortedTasks[taskIndex][0] <= time) {
-      const [enqueue, process, idx] = sortedTasks[taskIndex];
-      heap.push([process, idx]);
-      taskIndex++;
-    }
-
-    const [process, idx] = heap.pop();
-    time += process;
-    result.push(idx);
-  }
-
-  return result;
+    return ans;
 };
