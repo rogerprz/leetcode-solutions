c0cbc727756d092aed1a615c4cda4a76411e9bbe
Roger Perez <rogerprz@github.com>
2025-08-31 17:30:43 -0700

    Time: 212 ms (96.49%), Space: 95.9 MB (92.98%) - LeetHub

--- 1834-single-threaded-cpu/1834-single-threaded-cpu.js
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.js
@@ -1,10 +1,15 @@
 var getOrder = function (tasks) {
     if (!tasks || tasks.length === 0) return [];
     // sort by start time and task duration
+    // Input: tasks = [[1,2],[2,4],[3,2],[4,1]]
+    // Output: [0,2,3,1]
     const sortedTasks = tasks
-        .map((e, i) => { return { startTime: e[0], duration: e[1], index: i } })
+        .map((e, i) => { 
+            return { startTime: e[0], duration: e[1], index: i } 
+            })
         .sort((a, b) => a.startTime - b.startTime);
     // {startTime: x, duration, y, index: z}
+
     const minQueue = new PriorityQueue((a, b) => {
         if (a.duration === b.duration) return a.index - b.index;
         else return a.duration - b.duration;
@@ -14,13 +19,12 @@ var getOrder = function (tasks) {
     const ans = [];
     let i = 0;
     while (i < sortedTasks.length || !minQueue.isEmpty()) {
-        // next task if queue empty
         if (minQueue.isEmpty() && cpuStartTime < sortedTasks[i].startTime) {
             cpuStartTime = sortedTasks[i].startTime;
         }
         while (i < sortedTasks.length && cpuStartTime >= sortedTasks[i].startTime) {
             minQueue.enqueue(sortedTasks[i]);
-            i += 1;
+            i++
         }
 
         const nextTask = minQueue.dequeue();
