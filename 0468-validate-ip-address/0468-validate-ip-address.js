e4e22911d01b2171eb5d71499ee336260a7cb2ec
Roger Perez <rogerprz@github.com>
2025-09-06 12:13:28 -0700

    Time: 0 ms (100%), Space: 54.1 MB (21.74%) - LeetHub

---
+++ 0468-validate-ip-address/0468-validate-ip-address.js
@@ -0,0 +1,52 @@
+/**
+ * @param {string} queryIP
+ * @return {string}
+ */
+var validIPAddress = function(queryIP) {
+    if (queryIP.includes(".")) {
+        return verifyIp4Address(queryIP)
+    } else {
+        return verifyIp6Addreess(queryIP)
+    }
+};
+
+function verifyIp4Address(queryIP) {
+    const arr = queryIP.split(".")
+    if (arr.length != 4) return "Neither"
+    const letters ="qwertyuiopasdfghjklzxcvbnm";
+    for (const ip of arr) {
+        const num  = parseInt(ip)
+        const numLen = num.toString().length
+        if (numLen !== ip.length) {
+            return "Neither"
+        }
+        if (ip.startsWith("0")){
+            if (num.toString() != ip) {
+                return "Neither"
+            }
+        }
+        if (num < 0 || num > 255) {
+            return "Neither"
+        }
+    }
+    return "IPv4"  
+}
+
+function verifyIp6Addreess(queryIP) {
+    const arr = queryIP.split(":")
+    if (arr.length != 8) {
+        return "Neither"
+    }
+    const valid = "0123456789abcdefABCDEF"
+    for (const ip of arr) {
+        if (ip.length > 4 || ip.length < 1) {
+            return "Neither"
+        }
+        for (const elem of ip) {
+            if (!valid.includes(elem)) {
+                return 'Neither'
+            }
+        }
+    }
+    return "IPv6"
+}
