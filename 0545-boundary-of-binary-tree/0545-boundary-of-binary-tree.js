99f9ec367ef85d836b2d9d7a96b826674cab9fda
Roger Perez <rogerprz@github.com>
2025-09-06 12:28:06 -0700

    Time: 1 ms (89.09%), Space: 59.2 MB (70.91%) - LeetHub

--- 0545-boundary-of-binary-tree/0545-boundary-of-binary-tree.js
+++ 0545-boundary-of-binary-tree/0545-boundary-of-binary-tree.js
@@ -1,54 +1,54 @@
 /**
  * Definition for a binary tree node.
- * function TreeNode (val, left, right)  {
- *     this.val = (val===undefined ? 0 : val) 
- *     this.left = (left===undefined ? null : left) 
- *     this.right = (right===undefined ? null : right) 
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
  * }
  */
 /**
  * @param {TreeNode} root
  * @return {number[]}
  */
-var boundaryOfBinaryTree = function(root)  {
+var boundaryOfBinaryTree = function(root) {
     if (!root) return root 
-
-    const result = [];
+    const results = []
 
     const leftBorder = (node) => {
         while (node) {
-            if (node.left || node.right) result.push(node.val)
-
+            if (node.left || node.right) results.push(node.val)
             node = node.left || node.right
         }
     }
 
     const bottomBorder = (node) => {
-        if (!node) return node;
+        if (!node) return node 
 
         if (!node.left && !node.right) {
-            result.push(node.val)
-            return
+            results.push(node.val)
+            return;
         }
         bottomBorder(node.left)
         bottomBorder(node.right)
     }
-    const rightBorder = (node) => {
+
+    const rightBorder = (node) =>{
         const stack = []
+
         while (node) {
             if (node.left || node.right) stack.push(node.val)
             node = node.right || node.left
         }
-
         while (stack.length > 0) {
-            result.push(stack.pop())
+            results.push(stack.pop())
         }
     }
 
-    if (root.left || root.right) result.push(root.val)
+    if (root.left || root.right) results.push(root.val)
+
     leftBorder(root.left)
     bottomBorder(root)
     rightBorder(root.right)
 
-    return result 
+    return results
 };
