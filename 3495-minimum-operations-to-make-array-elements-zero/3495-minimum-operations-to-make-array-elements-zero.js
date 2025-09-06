7acd95ce4538e5ef043ab38f4d6819a5587e4dcd
Roger Perez <rogerprz@github.com>
2025-09-06 10:33:44 -0700

    Time: 45 ms (80%), Space: 86.8 MB (100%) - LeetHub

--- 3495-minimum-operations-to-make-array-elements-zero/3495-minimum-operations-to-make-array-elements-zero.js
+++ 3495-minimum-operations-to-make-array-elements-zero/3495-minimum-operations-to-make-array-elements-zero.js
@@ -2,25 +2,32 @@
  * @param {number[][]} queries
  * @return {number}
  */
+
 var minOperations = function(queries) {
-    let ans = 0n;
-    for (let [l, r] of queries) {
-        let S = 0n;
-        let dMax = 0n;
-        for (let k = 1; k <= 31; k++) {
-            let low = 1n << BigInt(k - 1);
-            let high = (1n << BigInt(k)) - 1n;
-            if (low > BigInt(r)) break;
-            let a = BigInt(Math.max(l, Number(low)));
-            let b = BigInt(Math.min(r, Number(high)));
-            if (a > b) continue;
-            let cnt = b - a + 1n;
-            let stepsForK = BigInt(Math.floor((k + 1) / 2));
-            S += cnt * stepsForK;
-            if (stepsForK > dMax) dMax = stepsForK;
-        }
-        let ops = dMax > (S + 1n) / 2n ? dMax : (S + 1n) / 2n;
-        ans += ops;
+    let limits = [1];
+    while (limits[limits.length - 1] <= 1e9) {
+        limits.push(limits[limits.length - 1] * 4);
     }
-    return Number(ans);
+    
+    let totalOps = 0;
+    
+    for (let [l, r] of queries) {
+        let sumSteps = 0;
+        for (let k = 1; k < limits.length; k++) {
+            let start = limits[k - 1];
+            let end = limits[k] - 1;
+            
+            if (r < start || l > end) continue;
+            
+            let overlapL = Math.max(l, start);
+            let overlapR = Math.min(r, end);
+            
+            let count = overlapR - overlapL + 1;
+            if (count > 0) sumSteps += count * k;
+        }
+        
+        totalOps += Math.ceil(sumSteps / 2);
+    }
+    
+    return totalOps;
 };
