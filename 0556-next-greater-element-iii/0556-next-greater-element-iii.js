8bbe25c1aab4c2bbabc602757032483ec0e60e68
Roger Perez <rogerprz@github.com>
2025-09-07 15:48:40 -0700

    Time: 0 ms (100%), Space: 54.2 MB (12.1%) - LeetHub

--- 0556-next-greater-element-iii/0556-next-greater-element-iii.js
+++ 0556-next-greater-element-iii/0556-next-greater-element-iii.js
@@ -2,24 +2,34 @@
  * @param {number} n
  * @return {number}
  */
-var nextGreaterElement = function(n) {
-    let digits = n.toString().split('');
-    let i = digits.length - 2;
-
-    while (i >= 0 && digits[i] >= digits[i + 1]) i--;
-    if (i === -1) return -1;
-
-    let j = digits.length - 1;
-    while (digits[j] <= digits[i]) j--;
-    [digits[i], digits[j]] = [digits[j], digits[i]];
-
-    let left = i + 1, right = digits.length - 1;
+function nextGreaterElement(n) {
+    const digits = n.toString().split('').map(Number);
+    const len = digits.length;
+    // Find pivot
+    let pivot = -1;
+    for (let i = len - 2; i >= 0; i--) {
+        if (digits[i] < digits[i + 1]) {
+            pivot = i;
+            break;
+        }
+    }
+    
+    if (pivot === -1) return -1;
+    // Find successor
+    for (let i = len - 1; i > pivot; i--) {
+        if (digits[i] > digits[pivot]) {
+            [digits[pivot], digits[i]] = [digits[i], digits[pivot]];
+            break;
+        }
+    }
+    // Two-pointer reverse suffix
+    let left = pivot + 1, right = len - 1;
     while (left < right) {
         [digits[left], digits[right]] = [digits[right], digits[left]];
         left++;
         right--;
     }
-
-    let result = parseInt(digits.join(''));
-    return result <= 2 ** 31 - 1 ? result : -1;
-};
+    
+    const result = parseInt(digits.join(''));
+    return result > 2147483647 ? -1 : result;
+}
