0f6f214bddccefcea43774053ed72a50fc05a73d
Roger Perez <rogerperez.us@gmail.com>
2025-09-27 14:27:08 -0700

    Time: 99 ms (13.24%), Space: 76.9 MB (7.65%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.js
+++ 0721-accounts-merge/0721-accounts-merge.js
@@ -3,21 +3,16 @@
  * @return {string[][]}
  */
 var accountsMerge = function(accounts) {
-    /**
-    // tempSet = [j1, j2]
-
- */
     const hash = {}
+    const emailToName = new Map()
     for (const account of accounts) {
         const tempStack = []
         const name = account[0]
         for (let i = 1; i < account.length; i++) {
             const email = account[i]
             if (!(email in hash)) {
-                hash[email] = {
-                    neighbors: new Set(),
-                    name
-                }
+                hash[email] = new Set()
+                emailToName.set(email, name)
             }
             tempStack.push(email)
         }
@@ -26,29 +21,24 @@ var accountsMerge = function(accounts) {
 
             for (let i = 1; i < account.length; i++) {
                 const email = account[i]
-                if (keyEmail === email) {
-                    continue
-                }
+                if (keyEmail === email) continue
                 
-                hash[keyEmail].neighbors.add(email)
+                hash[keyEmail].add(email)
             }
         }
-        // console.log('H:', hash)
     }
 
     const results = []
     const visited = new Set()
 
-    // visited = johnsmith, john_newyork, john00
-    // data = [johnsmith, john_newyork, john00]
     const dfs = (key, data) => {
         if (visited.has(key)) {
             return []
         }
         data.push(key)
         visited.add(key)
-        const content = hash[key]
-        for (const neighbor of content.neighbors) {
+        const neighbors = hash[key]
+        for (const neighbor of neighbors) {
             dfs(neighbor, data)
         }
         return data
@@ -57,44 +47,10 @@ var accountsMerge = function(accounts) {
     Object.keys(hash).forEach((key) => {
         if (!visited.has(key)){
             const emails = dfs(key, [])
-            const name = hash[key].name
+            const name = emailToName.get(key)
             results.push([name, ...emails.sort()])
         }
     })
 
     return results
 };
-/**
-    'johnsmith@mail.com':
-        neighbors:
-            'john_newyork@mail.com', 
-            'john00@mail.com'
-        name: 'John'
-    'john_newyork@mail.com':
-        neighbors:
-            'johnsmith@mail.com'
-        name: 'John'
-    'john00@mail.com':
-        neighbors:
-            'johnsmith@mail.com'
-        name: 'John'
-    'mary@mail.com':
-        neighbors:[]
-        name: 'Mary'
-    'johnnybravo@mail.com':
-        neighbors:[]
-        name: 'John'
-    [
-        ["John",["john00@mail.com","john_newyork@mail.com","johnsmith@mail.com"]],["Mary",["mary@mail.com"]],["John",["johnnybravo@mail.com"]]]
-   
-accounts = [
-    ["John","johnsmith@mail.com","john_newyork@mail.com"],
-    ["John","johnsmith@mail.com","john00@mail.com"],
-    ["Mary","mary@mail.com"],
-    ["John","johnnybravo@mail.com"]
-    ]
-Output: [
-    ["John","john00@mail.com","john_newyork@mail.com","johnsmith@mail.com"]
-    ["John","johnnybravo@mail.com"]]
-    ["Mary","mary@mail.com"],
-         */
