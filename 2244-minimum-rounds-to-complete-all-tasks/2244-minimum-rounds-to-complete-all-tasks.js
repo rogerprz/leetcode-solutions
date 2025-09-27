6278030947bb896a1c6f30c9e73d2dc2e680b83d
Roger Perez <rogerperez.us@gmail.com>
2025-09-27 15:15:53 -0700

    Time: 23 ms (89.36%), Space: 75.3 MB (34.04%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.js
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.js
@@ -5,12 +5,7 @@
 var minimumRounds = function(tasks) {
     const freq = new Map
     const nums = new Set()
-    // [2,2,3,3,2,4,4,4,4,4]
-    /**
-        2: 0
-        3: 0
-        4: 0
-     */
+
     for (const task of tasks) {
          freq.set(task, (freq.get(task) || 0) + 1)
     }
