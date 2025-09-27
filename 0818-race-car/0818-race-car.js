a5c7ec5fe4d6bbcda200921e3ac8a6f833c88156
Roger Perez <rogerperez.us@gmail.com>
2025-09-27 11:35:36 -0700

    Time: 14 ms (30.77%), Space: 58.6 MB (46.15%) - LeetHub

---
+++ 0818-race-car/0818-race-car.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} target
+ * @return {number}
+ */
+var racecar = function(target) {
+    const arr = [{moves: 0, pos: 0, speed: 1}]
+    let best = Infinity;
+
+    while (arr.length > 0) {
+        let { moves, pos, speed } = arr.pop();
+        if (pos === target) best = Math.min(best, moves)
+
+        if (moves >= best) continue
+        
+        arr.push({moves: moves+1, pos: pos+speed, speed: 2*speed});
+
+        if ((pos+speed>target && speed>0) || 
+            (pos+speed<target && speed<0)) {
+            arr.push({moves: moves+1, pos, speed: speed > 0 ? -1 : 1});
+        }
+    }
+
+    return best
+};
