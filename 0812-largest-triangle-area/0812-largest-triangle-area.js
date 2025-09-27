fa5a9bde634203e2548487be6a7ca7600ffb4e20
Roger Perez <rogerperez.us@gmail.com>
2025-09-27 12:44:03 -0700

    Time: 7 ms (78.21%), Space: 58.8 MB (53.85%) - LeetHub

--- 0812-largest-triangle-area/0812-largest-triangle-area.js
+++ 0812-largest-triangle-area/0812-largest-triangle-area.js
@@ -7,12 +7,13 @@ var largestTriangleArea = function(points) {
     const n = points.length;
 
     for (let i = 0; i < n; ++i) {
+        const [xI, yI] = points[i];
         for (let j = i + 1; j < n; ++j) {
+            const [xJ, yJ] = points[j];
+
             for (let k = j + 1; k < n; ++k) {
-                const [x1, y1] = points[i];
-                const [x2, y2] = points[j];
-                const [x3, y3] = points[k];
-                const area = 0.5 * Math.abs(x1*(y2 - y3) + x2*(y3 - y1) + x3*(y1 - y2));
+                const [xK, yK] = points[k];
+                const area = 0.5 * Math.abs(xI*(yJ - yK) + xJ*(yK - yI) + xK*(yI - yJ));
                 maxArea = Math.max(maxArea, area);
             }
         }
