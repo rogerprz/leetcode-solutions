3658e7a8151d0863e08da57388df410a15eee5b2
Roger Perez <rogerperez.us@gmail.com>
2025-09-27 12:10:51 -0700

    Time: 2 ms (46.38%), Space: 54.7 MB (59.42%) - LeetHub

---
+++ 0243-shortest-word-distance/0243-shortest-word-distance.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string[]} wordsDict
+ * @param {string} word1
+ * @param {string} word2
+ * @return {number}
+ */
+var shortestDistance = function(wordsDict, word1, word2) {
+    // wordsDict = ["practice", "makes", "perfect", "coding", "makes"], 
+    // word1 = "coding", 
+    // word2 = "practice"
+    const hash = {}
+
+    for (let i = 0; i < wordsDict.length; i++) {
+        const word = wordsDict[i]
+        if (word1 === word || word === word2) {
+            if (!(word in hash)) {
+                hash[word] = []
+            }
+            hash[word].push(i)
+        }
+    }
+
+    let min = Infinity;
+    // 1, 4 
+    // 3
+    for (const topWord of hash[word1]) { 
+        for (const botWord of hash[word2]) {
+            min = Math.min (Math.abs(topWord - botWord), min)
+        }
+    }
+    return min
+};
