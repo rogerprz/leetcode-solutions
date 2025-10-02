ab213bddd3185758fbf18d06bbe9bf5346753813
Roger Perez <rogerperez.us@gmail.com>
2025-10-02 12:20:21 -0700

    Time: 53 ms (34.31%), Space: 63.8 MB (38.69%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.js
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.js
@@ -2,46 +2,45 @@
  * @param {number[][]} matrix
  * @return {number}
  */
+ const directions = [[0,1], [0,-1], [1,0], [-1, 0]]
 var longestIncreasingPath = function(matrix) {
-    let map = new Map()
+    const m = matrix.length;
+    const n = matrix[0].length;
+    const memo = new Map();
 
-    const explore = (row, col) => {
-        const rowInbounds = row >= 0 && row < matrix.length;
-        const colInbounds = col >= 0 && col < matrix[0].length;
-
-        if (!rowInbounds || !colInbounds){
-            return 0
+    const dfs = (row, col) => {
+        const pos = `${row},${col}`
+        if (memo.has(pos)) {
+            return memo.get(pos) 
         }
-
-        const pos = `${row},${col}`;
-        if (map.has(pos)) {
-            return map.get(pos)
-        }
-
-        let maxPath = 1
+        let tempMax = 1;
         const currNum = matrix[row][col]
-        
-        const directions = [[0,1], [0,-1], [1,0], [-1,0]]
-        for (const [dr, dc] of directions) {
-            const newRow = row + dr
-            const newCol = col + dc
-            const rowInbounds = newRow >= 0 && newRow < matrix.length
-            const colInbounds = newCol >= 0 && newCol < matrix[0].length
+        for (const [rd, cd] of directions) {
+            const nextRow = row + rd;
+            const nextCol = col + cd;
+            const rowInbounds = nextRow >= 0 && nextRow < m;
+            const colInbounds = nextCol >= 0 && nextCol < n;
+
+            if (!rowInbounds || !colInbounds) {
+                continue;
+            }
+            const num = matrix[nextRow][nextCol]
+
+            if (num > currNum) {
+                const res = dfs(nextRow, nextCol) + 1
+                tempMax = Math.max(tempMax, res);
             
-            if (rowInbounds && colInbounds && matrix[newRow][newCol] > currNum) {
-                maxPath = Math.max(maxPath, 1 + explore(newRow, newCol))
             }
         }
-
-        map.set(pos, maxPath)
-        return maxPath
+        memo.set(pos, tempMax)
+        return tempMax;
     }
     
-    let max = 1
-    for (let row = 0; row < matrix.length; row++) {
-        for (let col = 0; col < matrix[0].length; col++) {
-            max = Math.max(max, explore(row, col))
+    let max = 0; 
+    for (let row = 0; row < m; row++) {
+        for (let col = 0; col < n; col++) {
+            max = Math.max(max, dfs(row, col))
         }
     }
-    return max
+    return max;
 };
