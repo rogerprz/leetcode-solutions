d2c709f1c4e86e08ec14af75fd03a22555180b82
Roger Perez <rogerperez.us@gmail.com>
2025-10-01 20:49:10 -0700

    Time: 81 ms (49.13%), Space: 82.3 MB (38.73%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.js
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.js
@@ -5,38 +5,36 @@
  * @return {number}
  */
 var jobScheduling = function(startTime, endTime, profit) {
-    const N = profit.length
-    const jobs = new Array(N)
+    const N = profit.length;
+    const jobs = new Array(N);
 
-    for (let i = 0; i< N; i++) {
+    for (let i = 0; i < N; i++) {
         jobs[i] = [endTime[i], startTime[i], profit[i]]
     }
 
     jobs.sort((a, b) => a[0] - b[0])
     const dp = new Array(N + 1).fill(0)
-
+    
     for (let i = 0; i < N; i++) {
-        const [end, start, profit] = jobs[i]
+        const [end, start, profit] = jobs[i];
 
-        const latestNonConflictJobIndex = upperBound(jobs, i, start);
-        dp[i + 1] = Math.max(dp[i], dp[latestNonConflictJobIndex] + profit);
+        // for each job we have to options;
+        // 1. skip it -> dp[i] same as previous state;
+        // 2. take it -> dp[latestNonCOnlifctJob] + profit[i];
+        // we do binary search to find the upperBound
+        let low = 0;
+        let high = i;
+        
+        while (low < high) {
+            const mid = Math.floor((low + high)/ 2)
+            const endJobTime = jobs[mid][0]
+            if (endJobTime <= start) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        dp[i + 1] = Math.max(dp[i], dp[low] + profit)
     }
-
     return dp[N]
 };
-
-function upperBound(jobs, endIndex, targetTime) {
-     let low = 0;
-    let high = endIndex;
-
-    while (low < high) {
-        const mid = Math.floor((low + high) / 2);
-        if (jobs[mid][0] <= targetTime) {
-            low = mid + 1;
-        } else {
-            high = mid;
-        }
-    }
-
-    return low;
-}
