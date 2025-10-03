6d3157487aa86f0912dbf36091249f2a58851ff6
Roger Perez <rogerperez.us@gmail.com>
2025-10-03 08:34:00 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.js
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.js
@@ -5,39 +5,40 @@
  * @return {number}
  */
 var jobScheduling = function(startTime, endTime, profit) {
+    if (startTime.length === 0) return 0;
     const N = profit.length;
     const jobs = new Array(N);
 
-    for (let i = 0; i < N; i++) {
+    for (let i = 0; i< N;i++) {
         jobs[i] = [endTime[i], startTime[i], profit[i]]
     }
 
-    jobs.sort((a, b) => a[0] - b[0])
-    const dp = new Array(N + 1).fill(0)
-    
+    jobs.sort((a,b) => a[0] - b[0]);
+
+    const dp = new Array(N+1).fill(0);
+    console.log("J:", jobs)
+    // [ 3, 1, 50 ], [ 4, 2, 10 ], [ 5, 3, 40 ], [ 6, 3, 70 ]
+
     for (let i = 0; i < N; i++) {
         const [end, start, profit] = jobs[i];
 
-        // for each job we have to options;
-        // 1. skip it -> dp[i] same as previous state;
-        // 2. take it -> dp[latestNonCOnlifctJob] + profit[i];
-        // we do binary search to find the upperBound
-        let low = 0;
-        let high = i;
-        
+        // we can skip it 
+        // take it 
+        // find the upper bounds using binary search
+        let low = 0; 
+        let high = 1; 
+
         while (low < high) {
-            const mid = Math.floor((low + high)/ 2)
+            const mid = Math.floor((low + high)/ 2);
             const endJobTime = jobs[mid][0]
-            if (endJobTime <= start) {
-                low = mid + 1;
-            } else {
+            // does the mid endJobTime <= start
+            if (start > endJobTime) {
                 high = mid;
+            } else {
+                low = mid + 1;
             }
         }
         dp[i + 1] = Math.max(dp[i], dp[low] + profit)
     }
-    return dp[N]
+    return dp[N];
 };
-
-
-    // [ 3, 1, 50 ], [ 4, 2, 10 ], [ 5, 3, 40 ], [ 6, 3, 70 ]
