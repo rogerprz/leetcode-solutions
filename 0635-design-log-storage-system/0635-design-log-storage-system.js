4bd30689298f27fa1177341e87ef7109a2b2c5be
Roger Perez <rogerperez.us@gmail.com>
2025-10-05 13:16:14 -0700

    Time: 23 ms (13.33%), Space: 63.3 MB (6.67%) - LeetHub

---
+++ 0635-design-log-storage-system/0635-design-log-storage-system.js
@@ -0,0 +1,57 @@
+
+var LogSystem = function() {
+    this.logs = [];
+};
+
+/** 
+ * @param {number} id 
+ * @param {string} timestamp
+ * @return {void}
+ */
+LogSystem.prototype.put = function(id, timestamp) {
+    this.logs.push([id, timestamp])
+};
+
+/** 
+ * @param {string} start 
+ * @param {string} end 
+ * @param {string} granularity
+ * @return {number[]}
+ */
+LogSystem.prototype.retrieve = function(start, end, granularity) {
+    const startTime = this.parse(start, granularity)
+    const endTime = this.parse(end, granularity)
+
+    const ans = []
+    
+    for (const [id, timestamp] of this.logs) {
+        const parsedTime = this.parse(timestamp, granularity)
+
+        if (parsedTime >= startTime && parsedTime <= endTime) {
+            ans.push(id)
+        }
+    }
+    return ans
+};
+
+LogSystem.prototype.parse = function(timestamp, granularity) {
+    const map = {
+      Year: 1,
+      Month: 2,
+      Day: 3,
+      Hour: 4,
+      Minute: 5,
+      Second: 6
+    }
+
+    const depth = map[granularity]
+
+    return timestamp.split(':').slice(0, depth).join(':')
+  }
+
+/** 
+ * Your LogSystem object will be instantiated and called as such:
+ * var obj = new LogSystem()
+ * obj.put(id,timestamp)
+ * var param_2 = obj.retrieve(start,end,granularity)
+ */
