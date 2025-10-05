e5b245d15104a83a096ae6956754711a735ab1a8
Roger Perez <rogerperez.us@gmail.com>
2025-10-05 15:50:56 -0700

    Time: 361 ms (19.93%), Space: 95.3 MB (10.73%) - LeetHub

--- 0827-making-a-large-island/0827-making-a-large-island.js
+++ 0827-making-a-large-island/0827-making-a-large-island.js
@@ -2,69 +2,74 @@
  * @param {number[][]} grid
  * @return {number}
  */
-const directions = [[0,1],[0,-1],[1,0],[-1,0]]
+const directions = [[0,1],[1,0], [-1,0], [0,-1]];
+
 var largestIsland = function(grid) {
-    let max = 0;
-    let map = new Map();
-    const N = grid.length;
-    const dfs = (r, c, islandId) => {
-        const queue = [[r, c]];
-        let count = 0;
-        while (queue.length) {
-            const [row, col] = queue.shift();
-            if (grid[row][col] !== 1) continue;
-            grid[row][col] = islandId;
-            count++;
-            for (const [dr, dc] of directions) {
-            const nextRow = row + dr
-            const nextCol = col + dc;
-            if (nextRow < 0 || nextRow >= N || nextCol < 0 || nextCol >= N) continue;
-            if (grid[nextRow][nextCol] === 1) queue.push([nextRow, nextCol]);
+    const M = grid.length;
+    const N = grid[0].length
+    const map = new Map()
+    const dfs = (row, col, islandId) => {
+
+        grid[row][col] = islandId
+        if (!map.has(islandId)) {
+            map.set(islandId, 0)
+        }
+        map.set(islandId,(map.get(islandId) || 0) + 1)
+
+        for (const [rd, cd] of directions) {
+            const nextRow = row + rd;
+            const nextCol = col + cd;
+
+            if (!inBounds(nextRow, nextCol, grid)){
+                continue
+            }
+            if (grid[nextRow][nextCol] === 1) {
+                dfs(nextRow, nextCol, islandId)
             }
         }
-        map.set(islandId, count);
-    };
-    let islandIdx = 1
-    for (let row = 0; row < grid.length;row++) {
-        for (let col = 0; col < grid[0].length;col++) {
-            if (grid[row][col] === 1) {
-                islandIdx++
-                dfs(row, col, islandIdx)
-            }
-        }    
     }
-    const zeroIsland = (r, c) => {
-        let maxIsland = 1
-        let visited = new Set()
-        for (const [rd, cd] of directions) {
-            const nextRow = r + rd;
-            const nextCol = c + cd;
-            if (
-                nextRow >= 0 && nextRow < grid.length &&
-                nextCol >= 0 && nextCol < grid[0].length
-            ) {
-                const neighborId = grid[nextRow][nextCol];
-                if (neighborId > 1 && !visited.has(neighborId)) {
-                    visited.add(neighborId);
-                    maxIsland += map.get(neighborId);
+    let islandId = 1
+    let zeroQueue = []
+    for (let row = 0; row < M; row++) {
+        for (let col = 0; col < N; col++) {
+            if (grid[row][col] === 1) {
+                islandId++
+                dfs(row, col, islandId)
+            } else if (grid[row][col] === 0) {
+                zeroQueue.push([row,col])
+            }
+        }
+    }
+    let max = 0;
+    for (let row = 0; row < M;row++) {
+       for (let col = 0; col < N;col++) {
+            let tempMax = 0;
+            let visited = new Set()
+            if (grid[row][col] !== 0) continue 
+            for (const [rd,cd] of directions) {
+                const nextRow = row + rd;
+                const nextCol = col + cd;
+                if (inBounds(nextRow, nextCol, grid)) {
+                    const next = grid[nextRow][nextCol]
+                    if (next > 1 && !visited.has(next)) {
+                        visited.add(next)
+                        tempMax += map.get(next)
+                    }
                 }
             }
-        }
-        return maxIsland
+            max = Math.max(max, tempMax + 1)
+       }
     }
-    /**
-    [2,2,0],
-    [0,0,3],
-    [0,0,3]
-    2: 2
-    3:1
-     */
-    for (let row = 0; row < grid.length;row++) {
-        for (let col = 0; col < grid[0].length;col++) {
-            if (grid[row][col] === 0) {
-                max = Math.max(max, zeroIsland(row, col))
-            }
-        }    
-    }
-    return max === 0 ? N * N : max
+
+    return max === 0 ? M * N : max;
 };
+
+function inBounds(row, col, grid) {
+    const rowInbounds = row < 0 || row >= grid.length;
+    const colInbounds = col < 0 || col >= grid[0].length;
+
+    if (rowInbounds || colInbounds) {
+        return false;
+    }
+    return true 
+}
