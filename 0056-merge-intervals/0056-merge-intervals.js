fe8cd14096a5453057ca8b0f456a1231c8dc9396
Roger Perez <rogerperez.us@gmail.com>
2025-10-05 14:06:29 -0700

    Time: 13 ms (25.41%), Space: 66.5 MB (19.83%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.js
+++ 0056-merge-intervals/0056-merge-intervals.js
@@ -3,24 +3,22 @@
  * @return {number[][]}
  */
 var merge = function(intervals) {
-    if (intervals.length <= 1) return intervals
+    if (intervals.length === 0) return intervals;
 
-    intervals.sort((a,b) => a[0] - b[0])
-    let prev = intervals[0]
-    let arr = [prev]
-    //          p 
-    // [[1,6], [8,10], ]
-    // [[1,3],[2,6],[8,10],[15,18]]
-    //               i
-    for (let i = 1; i < intervals.length;i++) {
+    const res = []
+    intervals.sort((a,b) => a[0]-b[0])
+    res.push(intervals[0])
+    // [[1,4],[4,5]]
+    // res = [[1,4]]
+    for (let i = 1; i < intervals.length; i++) {
+        // [4,5]
         const [start, end] = intervals[i]
-
-        if (start <= prev[1]) {
-            prev[1] = Math.max(prev[1], end)
+        const prevEnd = res[res.length - 1][1]
+        if (start <= prevEnd) {
+            res[res.length - 1][1] = Math.max(end, prevEnd)
         } else {
-            arr.push(intervals[i])
-            prev = intervals[i]
+            res.push(intervals[i])
         }
     }
-    return arr
+    return res
 };
