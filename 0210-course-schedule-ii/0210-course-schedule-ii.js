062c483ec4ae381f27446863314002257fee8a37
Roger Perez <rogerperez.us@gmail.com>
2025-10-08 13:28:25 -0700

    Time: 14 ms (36.53%), Space: 62.8 MB (29.41%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.js
+++ 0210-course-schedule-ii/0210-course-schedule-ii.js
@@ -5,6 +5,7 @@
  */
 var findOrder = function(numCourses, prerequisites) {
     const inDegree = new Array(numCourses).fill(0)
+
     const graph = {}
 
     for (const [course, preReq] of prerequisites) {
@@ -15,20 +16,23 @@ var findOrder = function(numCourses, prerequisites) {
         inDegree[course]++
     }
 
-    const res = []
+    const res = [];
     const queue = []
 
-    for (let i = 0; i < inDegree.length; i++) {
+    for (let i = 0; i < inDegree.length;i++) {
         if (inDegree[i] === 0) queue.push(i)
     }
+
     while (queue.length > 0) {
         let preNum = queue.shift();
+
         res.push(preNum)
-        if (!(preNum in graph)) continue
+
+        if (!(preNum in graph)) continue; 
 
         for (const course of graph[preNum]) {
             inDegree[course]--
-            if (inDegree[course] === 0) queue.push(course)
+            if( inDegree[course] === 0) queue.push(course) 
         }
     }
     return res.length === numCourses ? res : [];
