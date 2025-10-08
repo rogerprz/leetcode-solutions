19fd7886a5265b89a3e9f61cca8278db0d25a6d1
Roger Perez <rogerperez.us@gmail.com>
2025-10-08 13:18:39 -0700

    Time: 10 ms (66.67%), Space: 60.2 MB (83.9%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.js
+++ 0210-course-schedule-ii/0210-course-schedule-ii.js
@@ -4,33 +4,32 @@
  * @return {number[]}
  */
 var findOrder = function(numCourses, prerequisites) {
-    const graph = {}
     const inDegree = new Array(numCourses).fill(0)
+    const graph = {}
 
-    for (const [course, pre] of prerequisites) {
-        if (!(pre in graph)) graph[pre] = []
-        graph[pre].push(course)
+    for (const [course, preReq] of prerequisites) {
+        if (!(preReq in graph)) {
+            graph[preReq] = []
+        }
+        graph[preReq].push(course)
         inDegree[course]++
     }
 
-    let res = []
-    let queue = []
+    const res = []
+    const queue = []
 
-    for (let i = 0; i < numCourses; i++) {
+    for (let i = 0; i < inDegree.length; i++) {
         if (inDegree[i] === 0) queue.push(i)
     }
-
     while (queue.length > 0) {
-        const curr = queue.shift()
+        let preNum = queue.shift();
+        res.push(preNum)
+        if (!(preNum in graph)) continue
 
-        res.push(curr)
-
-        if (!(curr in graph)) continue
-
-        for (const neighbor of graph[curr]) {
-            inDegree[neighbor]--
-            if (inDegree[neighbor] === 0) queue.push(neighbor)
+        for (const course of graph[preNum]) {
+            inDegree[course]--
+            if (inDegree[course] === 0) queue.push(course)
         }
     }
-    return res.length === numCourses ? res : []
+    return res.length === numCourses ? res : [];
 };
