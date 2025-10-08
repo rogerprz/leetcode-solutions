63b93ecf5e4e72b6caf2896b563255cc2d824467
Roger Perez <rogerperez.us@gmail.com>
2025-10-07 22:06:45 -0700

    Time: 81 ms (96.81%), Space: 85.9 MB (29.79%) - LeetHub

--- 1353-maximum-number-of-events-that-can-be-attended/1353-maximum-number-of-events-that-can-be-attended.js
+++ 1353-maximum-number-of-events-that-can-be-attended/1353-maximum-number-of-events-that-can-be-attended.js
@@ -1,29 +1,23 @@
-/**
- * @param {number[][]} events
- * @return {number}
- */
-var maxEvents = function(events) {
-  events.sort((a,b) => a[1] - b[1]);
-  const maxDay = events.at(-1)[1];
-  const days = new Array(maxDay + 1).fill(0).map((_, i) => i);
+function maxEvents(events) {
+  events.sort((a, b) => a[1] - b[1]);
 
-  const search = (index) => {
-    if (days[index] !== index) {
-      days[index] = search(days[index]);
-    }
-    return days[index];
-  }
-
+  const parent = new Map();
   let count = 0;
-  for(const [start, end] of events) {
-    const day = search(start);
-    if (day <= end) {
-      count += 1;
-      days[day] = search(day + 1);
+
+  function find(x) {
+    if (!parent.has(x)) return x;
+    const next = find(parent.get(x));
+    parent.set(x, next);
+    return next;
+  }
+
+  for (const [start, end] of events) {
+    const available = find(start);
+    if (available <= end) {
+      count++;
+      parent.set(available, available + 1); // mark as used
     }
   }
+
   return count;
-
-
-    
-};
+}
