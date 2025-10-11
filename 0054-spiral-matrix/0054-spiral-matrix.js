6bf4adba79effdce8f3e8b530dd006301b81d3d0
Roger Perez <rogerperez.us@gmail.com>
2025-10-11 13:54:06 -0700

    Time: 0 ms (100%), Space: 54.2 MB (29.73%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.js
+++ 0054-spiral-matrix/0054-spiral-matrix.js
@@ -4,7 +4,6 @@
  */
 var spiralOrder = function(matrix) {
     const res = [];
-
     if (matrix.length === 0) return res;
 
     let top = 0;
@@ -13,35 +12,28 @@ var spiralOrder = function(matrix) {
     let right = matrix[0].length - 1;
 
     while (top <= bot && left <= right) {
-        /**
-         0,1,2
-        [1,2,3], 0
-        [4,5,6], 1 
-        [7,8,9], 2
-         */
-        for (let i = left; i <= right;i++) {
+        for (let i = left; i <= right; i++) {
             res.push(matrix[top][i])
         }
         top++
-        for (let i = top; i <= bot;i++) {
+
+        for (let i = top; i <= bot; i++) {
             res.push(matrix[i][right])
         }
         right--
-        
+
         if (top <= bot) {
             for (let i = right; i >= left;i--) {
                 res.push(matrix[bot][i])
             }
             bot--
         }
-
         if (left <= right) {
-            for (let i = bot; i >= top;i--) {
+            for (let i = bot; i >= top; i--) {
                 res.push(matrix[i][left])
             }
             left++
         }
-
     }
     return res;
 };
