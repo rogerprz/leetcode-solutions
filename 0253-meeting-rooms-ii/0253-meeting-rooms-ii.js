d14f00037c3eb6712bea19f662b9c3b8b07f3564
Roger Perez <rogerperez.us@gmail.com>
2025-10-11 12:55:40 -0700

    Time: 17 ms (28.32%), Space: 59.8 MB (39.8%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.js
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.js
@@ -3,21 +3,21 @@
  * @return {number}
  */
 var minMeetingRooms = function(intervals) {
-    const sTimes = intervals.sort((a,b) => a[0]- b[0]).map((val) => val[0])
-    const eTimes = intervals.sort((a,b) => a[1]- b[1]).map((val) => val[1]) 
-    console.log("S", sTimes)
-    console.log("E", eTimes)
-    let count = 0;
+    let startTimes = intervals.map((([start,_end]) => start)).sort((a,b) => a - b)
+    let endTimes = intervals.map((([_start,end]) => end)).sort((a,b) => a - b)
+    console.log("S:", startTimes)
+    console.log("E:", endTimes)
+
+    let count = 0; // 2
     let start = 0;
     let end = 0;
-
-    while (start < intervals.length) {
-        if (sTimes[start] < eTimes[end]) {
+    while (start < startTimes.length) {
+        if (startTimes[start] < endTimes[end]) {
             count++
         } else {
             end++
         }
         start++
     }
-    return count
+    return count;
 };
