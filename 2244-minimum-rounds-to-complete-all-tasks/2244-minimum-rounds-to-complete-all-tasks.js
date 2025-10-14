1d358a0e868cd903f96d3076c07392c98b754f67
Roger Perez <rogerperez.us@gmail.com>
2025-10-14 10:06:21 -0700

    Time: 88 ms (5.71%), Space: 71.6 MB (80%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.js
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.js
@@ -2,9 +2,13 @@
  * @param {number[]} tasks
  * @return {number}
  */
+/**
+ * @param {number[]} tasks
+ * @return {number}
+ */
 var minimumRounds = function(tasks) {
     const freq = {}
-    const nums = new Set()
+    const keys = new Set()
     // [2,2,3,3,2,4,4,4,4,4]
     /**
         2: 0
@@ -14,33 +18,35 @@ var minimumRounds = function(tasks) {
     for (const task of tasks) {
         if (!(task in freq)) {
             freq[task] = 0
-            nums.add(task)
+            keys.add(task)
         }
         freq[task]++
     }
 
     let rounds = 0; // 3
 
-    nums.forEach((num) => {
-        if (rounds !== -1){
-             
-        while (freq[num] > 0) {
-            const count = freq[num]
-
-            if (count === 1) {
-                rounds = -1
-                break;
-            }
-            const isOdd = count % 2 === 1
-            if (isOdd || count > 5 ) {
-                freq[num]-=3
-            } else {
-                freq[num] -=2
-            }
-            rounds++
+    for (const key of keys.values()) {
+        if (rounds == -1){
+            break;
         }
-    }
+            while (freq[key] > 0) {
+                let count = freq[key]
+
+                if (count === 1) {
+                    rounds = -1;
+                    break;
+                }
+                const isOdd = count % 2 === 1
+
+                if (isOdd || count > 5) {
+                    freq[key] -= 3
+                } else {
+                    freq[key] -= 2
+                }
+                rounds++
+            }
         
-    })
+        
+    }
     return rounds;
 };
