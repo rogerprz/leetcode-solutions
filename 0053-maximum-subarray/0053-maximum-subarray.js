9de697d86ce55e87bed8f1f77e7138bb1fde3322
Roger Perez <rogerperez.us@gmail.com>
2025-10-21 12:39:53 -0700

    Time: 3 ms (41.9%), Space: 66.7 MB (5.09%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    let dp = [...nums];    
+    let max = dp[0];
+    for(let i=1; i<nums.length; i++) {
+        dp[i] = Math.max(dp[i-1] + nums[i], nums[i]);
+        max = Math.max(max, dp[i]);
+    } 
+    return max;
+};
