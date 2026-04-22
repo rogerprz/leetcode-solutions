9414d093c6e40414c09fbb60d810d8ca8a301dd3
Roger Perez <rogerperez.us@gmail.com>
2026-04-22 13:36:34 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
@@ -5,9 +5,7 @@
 var findMin = function(nums) {
     let l = 0;
     let r = nums.length - 1
-    //. 0,1,2,3,4
-    // [3,4,5,1,2]
-    //        lm r
+
     while (l < r) {
         let m = Math.floor((l+r)/2)
         const low = nums[l]
